60dc49e3a4a0559763625e32c32ae0914072aa2a
Milan <milankloewer@gmx.de>
2021-12-21 16:49:02 +0000

    time integration structure

--- src/time_integration.jl
+++ src/time_integration.jl
@@ -144,3 +144,33 @@ function timestep!( Prog::PrognosticVariables{NF},  # all prognostic variables
     leapfrog!(humid,humid_tend,dt,l1,C)
 end
 
+"""Calculate a single time step for SpeedyWeather.jl"""
+function time_stepping!(Prog::PrognosticVariables{NF},  # all prognostic variables
+                        Diag::PrognosticVariables{NF},  # all pre-allocated diagnostic variables
+                        C::Constants{NF},               # struct containing constants
+                        G::GeoSpectral{NF},             # struct containing geometry and spectral transform constants
+                        HD::HorizontalDiffusion{NF}     # struct containing horizontal diffusion constants
+                        P::Params                       # struct containing all model parameters
+                        ) where {NF<:AbstractFloat}
+    
+    @unpack n_timesteps, Δt = C
+    @unpack output = P
+
+    # FEEDBACK, OUTPUT INITIALISATION AND STORING INITIAL CONDITIONS
+    # feedback = feedback_initialise(S)
+    # ncfile = output_initialise(feedback,S)
+
+    first_timestep!(Prog,Diag,C,G,HD)
+
+    for i in 1:n_timesteps
+        timestep!(Prog,Diag,2,2,2Δt,C,G,HD)
+
+        # FEEDBACK AND OUTPUT
+        # feedback.i = i
+        # feedback!(Prog,feedback,S)
+        # output_nc!(i,netCDFfiles,Prog,Diag,S)
+    end
+
+    return Prog
+end
+    
