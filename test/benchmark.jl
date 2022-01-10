5b6e8102a506946508f5e8188eb75f7b5df48a4e
Milan <milankloewer@gmx.de>
2022-01-10 13:01:59 +0000

    spectral transform benchmark added

---
+++ test/benchmark.jl
@@ -0,0 +1,16 @@
+using Test, BenchmarkTools
+include("../src/SpeedyWeather.jl")
+using .SpeedyWeather
+
+# original speedy T30 with 96x48 grid
+P = Parameters(NF=Float64;trunc=30,nlat=48,nlon=96)
+G = GeoSpectral{P.NF}(P)
+B = Boundaries{P.NF}(P,G)
+
+geopot_surf_spectral = B.geopot_surf
+geopot_surf_grid = gridded(geopot_surf_spectral,G)
+geopot_surf_fourier = fourier(geopot_surf_grid,G)
+
+@btime fourier_inverse(fourier($geopot_surf_grid,$G),$G);
+@btime legendre_inverse(legendre($geopot_surf_fourier,$G),$G);
+@btime gridded(spectral($geopot_surf_grid,$G),$G);
