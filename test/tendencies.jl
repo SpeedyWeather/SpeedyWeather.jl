b72032945d504e1bc9ddd862f6045a11154be693
Tom Kimpson <tomkimpson@gmail.com>
2022-03-30 17:39:10 +0100

    clean up

--- test/tendencies.jl
+++ test/tendencies.jl
@@ -5,6 +5,7 @@ using Test
 @testset "Consistent run through of tendencies" begin
 
     #No tests, just make sure get_tendencies runs without bugs
+    #No values calculated yet, need consistent definitions of grads, spectral transforms etc.
     NF = Float64 
     P,D,M = SpeedyWeather.initialize_model(NF)
     l2 = 2
