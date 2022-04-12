1a85529df9c71cd31cfed066407fe885e35cb773
Milan <milankloewer@gmx.de>
2022-04-12 13:36:30 +0100

    run_speedy with initialize_model separated

--- test/tendencies.jl
+++ test/tendencies.jl
@@ -1,14 +1,9 @@
-using SpeedyWeather
-using Test
-
-
 @testset "Consistent run through of tendencies" begin
-
-    #No tests, just make sure get_tendencies runs without bugs
-    #No values calculated yet, need consistent definitions of grads, spectral transforms etc.
-    NF = Float64 
-    P,D,M = SpeedyWeather.initialize_model(NF)
-    l2 = 2
-    SpeedyWeather.get_tendencies!(P,D,l2,M) 
-    
+    # No tests, just make sure get_tendencies runs without bugs
+    # No values calculated yet, need consistent definitions of grads, spectral transforms etc.
+    for NF in (Float32,Float64)
+        P,D,M = SpeedyWeather.initialize_model(NF)
+        l2 = 2
+        SpeedyWeather.get_tendencies!(P,D,l2,M)
+    end    
 end
