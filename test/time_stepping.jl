5902a23ddbb3b9a98e2a7d550648eb8e704e4ff7
Milan <milankloewer@gmx.de>
2022-04-27 15:44:45 +0100

    time int test+Float16 but nlev nleapfrog swapped

--- test/time_stepping.jl
+++ test/time_stepping.jl
@@ -9,24 +9,26 @@
     n_timesteps = round(Int,2π*n_rotations/(ω*Δt))
 
     # loop over different precisions
-    for NF in (Float32,Float64)
+    for NF in (Float16,Float32,Float64)
         P = Parameters(NF=NF)
         C = Constants(P)
 
         # INITIAL CONDITIONS
-        # with 3x3x2 some spectral coeffs, x2 for leapfrog dimension
+        # with lmax x mmax x nleapfrog x nlev
         X = ones(Complex{NF},3,3,2,2)
         
         # store only 1 of the 3x3x2 values (all the same) per time step
         Xout = zeros(Complex{NF},n_timesteps+1)
 
         # exact 2nd leapfrog step 
-        X[:,:,:,2] = X[:,:,:,1]*Complex{NF}(exp(im*ω*Δt))
+        X[:,:,2,:] = X[:,:,1,:]*Complex{NF}(exp(im*ω*Δt))
         Xout[1] = X[1,1,1,1]      # store initial conditions
 
         # leapfrog forward
+        lf = 2  # leapfrog index to be used for tendency
         for i in 2:n_timesteps+1
-            SpeedyWeather.leapfrog!(X,F.(X[:,:,:,2],NF(ω)),NF(2Δt),C)
+            # always evaluate F with lf=2
+            SpeedyWeather.leapfrog!(X,F.(X[:,:,lf,:],NF(ω)),NF(2Δt),C)
             Xout[i] = X[1,1,1,1]
         end
 
@@ -40,12 +42,12 @@
     n_rotations = 10
     n_timesteps = round(Int,2π*n_rotations/(ω*Δt))
 
-    for NF in (Float32,Float64)
+    for NF in (Float16,Float32,Float64)
         P = Parameters(NF=NF)
         C = Constants(P)
 
         # INITIAL CONDITIONS
-        # with 3x3 some spectral coeffs, x2 for leapfrog dimension
+        # with lmax x mmax x nleapfrog
         X = ones(Complex{NF},3,3,2)
         
         # store only 1 of the 3x3 values (all the same) per time step
@@ -56,8 +58,9 @@
         Xout[1] = X[1,1,1]      # store initial conditions
 
         # leapfrog forward
+        lf = 2  # leapfrog index to be used for tendency
         for i in 2:n_timesteps+1
-            SpeedyWeather.leapfrog!(X,F.(X[:,:,2],NF(ω)),NF(2Δt),C)
+            SpeedyWeather.leapfrog!(X,F.(X[:,:,lf],NF(ω)),NF(2Δt),C)
             Xout[i] = X[1,1,1]
         end
 
@@ -82,7 +85,7 @@
 
         # leapfrog forward
         for i in 2:n_timesteps+1
-            SpeedyWeather.leapfrog!(X,F.(X[:,:,2],NF(ω)),NF(2Δt),C)
+            SpeedyWeather.leapfrog!(X,F.(X[:,:,lf],NF(ω)),NF(2Δt),C)
             Xout[i] = X[1,1,1]
         end
 
