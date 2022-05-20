b81c4af44231d00008f90f2bf3a34fcbb0044a4e
Alistair White <alistair@pik-potsdam.de>
2022-05-20 20:46:34 +0200

    Make coefficient structs parametric

--- src/coefficient_structs.jl
+++ src/coefficient_structs.jl
@@ -7,14 +7,14 @@ See geometry.jl and function vertical_coordinate for more informaiton.
 Following the notation of https://en.wikipedia.org/wiki/Generalised_logistic_function (Dec 15 2021).
 
 Change default parameters for more/fewer levels in the stratosphere vs troposphere vs boundary layer."""
-@with_kw struct GenLogisticCoefs <: Coefficients
-    A::Real = -0.283     # obtained from a fit in /input_date/vertical_coordinate/vertical_resolution.ipynb
-    K::Real = 0.871
-    C::Real = 0.414
-    Q::Real = 6.695
-    B::Real = 10.336
-    M::Real = 0.602
-    ν::Real = 5.812
+@with_kw struct GenLogisticCoefs{NF<:Real} <: Coefficients
+    A::NF = -0.283     # obtained from a fit in /input_date/vertical_coordinate/vertical_resolution.ipynb
+    K::NF = 0.871
+    C::NF = 0.414
+    Q::NF = 6.695
+    B::NF = 10.336
+    M::NF = 0.602
+    ν::NF = 5.812
 end
 
 """
@@ -23,11 +23,11 @@ Parameters for computing saturation specific humidity.
 These parameters, and the formula where they are used, do not appear in the original
 Speedy documentation.
 """
-@with_kw struct HumidityCoefs <: Coefficients
-    e₀::Real = 6.108e-3
-    C₁::Real = 17.269
-    C₂::Real = 21.875
-    T₀::Real = 273.16
-    T₁::Real = 35.86
-    T₂::Real = 7.66
+@with_kw struct HumidityCoefs{NF<:Real} <: Coefficients
+    e₀::NF = 6.108e-3
+    C₁::NF = 17.269
+    C₂::NF = 21.875
+    T₀::NF = 273.16
+    T₁::NF = 35.86
+    T₂::NF = 7.66
 end
