76a6518a10cdfcb28dfffe9a4e3d5e96b809ed26
Alistair White <alistair@pik-potsdam.de>
2022-05-30 14:45:04 +0200

    Add tests

--- test/humidity.jl
+++ test/humidity.jl
@@ -1,3 +1,30 @@
+import Parameters: @unpack
+using Distributions
+
 @testset "humidity.jl" begin
-    # Placeholder
+    @testset "get_saturation_vapour_pressure!" begin
+        _, diag, model = SpeedyWeather.initialize_speedy()
+        @unpack nlon, nlat, nlev = model.geospectral.geometry
+        @unpack sat_vap_pressure = diag.parametrization_variables
+
+        temp_grid = rand(Uniform(200, 350), nlon, nlat, nlev)  # Typical values in K
+
+        SpeedyWeather.get_saturation_vapour_pressure!(sat_vap_pressure, temp_grid, model)
+
+        @test all(sat_vap_pressure .> 0.0) && all(sat_vap_pressure .< 500.0)
+    end
+
+    @testset "get_saturation_specific_humidity!" begin
+        _, diag, model = SpeedyWeather.initialize_speedy()
+        @unpack nlon, nlat, nlev = model.geospectral.geometry
+        @unpack sat_vap_pressure, sat_spec_humidity = diag.parametrization_variables
+
+        temp_grid = rand(Uniform(200, 350), nlon, nlat, nlev)       # Typical values in K
+        pres_grid = rand(Uniform(log(300), log(2000)), nlon, nlat)  # Typical values in log(hPa)
+
+        SpeedyWeather.get_saturation_specific_humidity!(sat_spec_humidity, sat_vap_pressure, temp_grid, pres_grid, model)
+
+        @test all(isfinite.(sat_spec_humidity))
+        @test !any(iszero.(sat_spec_humidity))
+    end
 end
---
+++ test/large_scale_condensation.jl
@@ -0,0 +1,10 @@
+using Parameters
+
+@testset "large_scale_condensation.jl" begin
+    @testset "get_large_scale_condensation_tendencies!" begin
+        _, diag, model = SpeedyWeather.initialize_speedy()
+
+        # For now, just check that it runs without errors
+        SpeedyWeather.get_large_scale_condensation_tendencies!(diag, model)
+    end
+end
