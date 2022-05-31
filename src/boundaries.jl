704ad96ac527f7639fb99b0bdbae02357dfd409e
Milan <milankloewer@gmx.de>
2022-05-31 18:25:24 +0200

    load orography only for >BarotropicModel

--- src/boundaries.jl
+++ src/boundaries.jl
@@ -17,28 +17,37 @@ orography, land-sea mask and albedo from an netCDF file and stores the in a
 `Boundaries` struct."""
 function Boundaries(P::Parameters)
 
-    @unpack orography_path, orography_file, gravity, NF = P
+    if P.model == :barotropic   # no boundary data needed with the barotropic model
+        # create dummy arrays
+        geopot_surf         = zeros(complex(P.NF),1,1)
+        geopot_surf_grid    = zeros(P.NF,1,1)
+    
+    else                        # load orography for the shallowwater model
+                                # TODO: change for primitive equation model accordingly
 
-    # LOAD NETCDF FILE
-    if orography_path == ""
-        path = joinpath(@__DIR__,"../input_data",orography_file)
-    else
-        path = joinpath(orography_path,orography_file)
+        @unpack orography_path, orography_file, gravity = P
+
+        # LOAD NETCDF FILE
+        if orography_path == ""
+            path = joinpath(@__DIR__,"../input_data",orography_file)
+        else
+            path = joinpath(orography_path,orography_file)
+        end
+        ncfile = NetCDF.open(path)
+        
+        # READ, TODO check which latitude ordering is required, it's North to South in file
+        orography = ncfile.vars["orog"][:,:]        # height [m]
+        # landsea_mask = ncfile.vars["lsm"][:,:]    # fraction of land [0-1]
+        # albedo = ncfile.vars["alb"][:,:]          # annual-mean albedo [0-1]
+
+        # GEOPOTENTIAL, transform to spectral space and truncate accordingly   
+        geopot_surf_highres = spectral(gravity*orography)
+        geopot_surf = spectral_truncation(geopot_surf_highres,  # truncate to
+                                            P.trunc+1,P.trunc)  # lmax+1,mmax matrix
+        spectral_truncation!(geopot_surf,P.trunc)               # but set l=lmax+1 row to zero
+        geopot_surf_grid = gridded(geopot_surf)
     end
-    ncfile = NetCDF.open(path)
-    
-    # READ, TODO check which latitude ordering is required, it's North to South in file
-    orography = ncfile.vars["orog"][:,:]        # height [m]
-    # landsea_mask = ncfile.vars["lsm"][:,:]    # fraction of land [0-1]
-    # albedo = ncfile.vars["alb"][:,:]          # annual-mean albedo [0-1]
 
-    # GEOPOTENTIAL, transform to spectral space and truncate accordingly   
-    geopot_surf_highres = spectral(gravity*orography)
-    geopot_surf = spectral_truncation(geopot_surf_highres,  # truncate to
-                                        P.trunc+1,P.trunc)  # lmax+1,mmax matrix
-    spectral_truncation!(geopot_surf,P.trunc)               # but set l=lmax+1 row to zero
-    geopot_surf_grid = gridded(geopot_surf)
-    
     # convert to number format NF here
-    return Boundaries{NF}(geopot_surf_grid,geopot_surf) #,landsea_mask,albedo)
+    return Boundaries{P.NF}(geopot_surf_grid,geopot_surf) #,landsea_mask,albedo)
 end
