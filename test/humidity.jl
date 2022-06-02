ce7302899005b781e4638d5b4c27022fdfc23b99
Alistair White <alistair@pik-potsdam.de>
2022-06-02 11:59:49 +0200

    Fix NF in tests

--- test/humidity.jl
+++ test/humidity.jl
@@ -1,10 +1,11 @@
 @testset "humidity.jl" begin
     @testset "get_saturation_vapour_pressure!" begin
-        _, diag, model = SpeedyWeather.initialize_speedy()
+        NF = Float32
+        _, diag, model = SpeedyWeather.initialize_speedy(NF)
         (;nlon, nlat, nlev) = model.geospectral.geometry
         (;sat_vap_pressure) = diag.parametrization_variables
 
-        temp_grid = 200 .+ 150 * rand(nlon, nlat, nlev)  # Typical values between 200-350K
+        temp_grid = 200 .+ 150 * rand(NF, nlon, nlat, nlev)  # Typical values between 200-350K
 
         SpeedyWeather.get_saturation_vapour_pressure!(sat_vap_pressure, temp_grid, model)
 
@@ -12,12 +13,13 @@
     end
 
     @testset "get_saturation_specific_humidity!" begin
-        _, diag, model = SpeedyWeather.initialize_speedy()
+        NF = Float32
+        _, diag, model = SpeedyWeather.initialize_speedy(NF)
         (;nlon, nlat, nlev) = model.geospectral.geometry
         (;sat_vap_pressure, sat_spec_humidity) = diag.parametrization_variables
 
-        temp_grid = 200 .+ 150 * rand(nlon, nlat, nlev)  # Typical values between 200-350 K
-        pres_grid = 300 .* 1700 * rand(nlon, nlat)       # Typical values between 300-2000 hPa
+        temp_grid = 200 .+ 150 * rand(NF, nlon, nlat, nlev)  # Typical values between 200-350 K
+        pres_grid = 300 .* 1700 * rand(NF, nlon, nlat)       # Typical values between 300-2000 hPa
 
         SpeedyWeather.get_saturation_specific_humidity!(sat_spec_humidity, sat_vap_pressure, temp_grid, pres_grid, model)
 
