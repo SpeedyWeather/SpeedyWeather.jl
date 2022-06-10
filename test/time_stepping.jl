d5c452d68aaa960b703bd8c46b274480c916c233
T <t1@Ts-MacBook-Pro.local>
2022-06-10 15:04:02 +0100

    Include posits

--- test/time_stepping.jl
+++ test/time_stepping.jl
@@ -1,4 +1,7 @@
 using StochasticRounding
+using DoubleFloats
+using SoftPosit
+
 
 @testset "Leapfrog oscillation" begin
     
@@ -11,7 +14,7 @@ using StochasticRounding
     n_timesteps = round(Int,2π*n_rotations/(ω*Δt))
 
     # loop over different precisions
-    for NF in (Float16sr,Float16,Float32,Float64)
+    for NF in (Float16sr,Float16,Float32,Float64,Posit16,Posit32)
         P = Parameters(NF=NF)
         C = Constants(P)
 
@@ -26,21 +29,22 @@ using StochasticRounding
         X[:,:,2] = X[:,:,1]*Complex{NF}(exp(im*ω*Δt))
         Xout[1] = X[1,1,1]      # store initial conditions
 
+
         # leapfrog forward
         lf = 2  # leapfrog index to be used for tendency
         for i in 2:n_timesteps+1
             SpeedyWeather.leapfrog!(X,F.(X[:,:,lf],NF(ω)),NF(2Δt),C)
             Xout[i] = X[1,1,1]
             
-            if i % n_rotations == 0
-               println(Xout[i], ' ', abs(Xout[i]))
-            end 
+            # if i % n_rotations == 0
+            #    println(Xout[i], ' ', abs(Xout[i]))
+            # end 
 
         end
         #println(Xout)
         # Error in the magnitude - ideally should always be = 1 if on unit circle
-        error = abs(Xout[end]) - 1.0
-        println("Error after " * string(n_timesteps) * " timesteps for NF " * string(NF) * " is: ", error)
+        error = abs(Xout[end]) #- Real{NF}(1.0)
+        println("Magnitude after " * string(n_timesteps) * " timesteps for NF " * string(NF) * " is: ", error)
 
     end
 end
