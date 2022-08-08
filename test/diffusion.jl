68e9497c8dfa11199537dbcf150a87959e8d2d8d
Milan <milankloewer@gmx.de>
2022-08-08 18:25:01 +0100

    diffusion test updated

--- test/diffusion.jl
+++ test/diffusion.jl
@@ -3,42 +3,35 @@
 
         p,d,m = initialize_speedy(T)
 
-        (;vor) = p
-        (;vor_tend) = d.tendencies
+        (;vor) = p.layers[1].leapfrog[1]
+        (;vor_tend) = d.layers[1].tendencies
         (;damping,damping_impl) = m.horizontal_diffusion
 
         vor       .= randn(Complex{T},size(vor)...)
-        vor_tend  .= zeros(Complex{T},size(vor_tend)...)
 
-        vor_lf1 = view(vor,:,:,1,:)
-
-        SpeedyWeather.horizontal_diffusion!(vor_tend,vor_lf1,damping,damping_impl)
-
-        (;nlev) = m.geospectral.geometry
-        (;lmax,mmax) = m.geospectral.spectral_transform
+        SpeedyWeather.horizontal_diffusion!(vor_tend,vor,damping,damping_impl)
 
         # diffusion tendency has opposite sign (real/imag respectively)
         # than prognostic variable to act as a dissipation 
-        for k in [1]
-            for m in 1:mmax+1
-                for l in max(2,m):lmax+1
-                    @test -sign(real(vor_lf1[l,m,k])) == sign(real(vor_tend[l,m,k]))
-                    @test -sign(imag(vor_lf1[l,m,k])) == sign(imag(vor_tend[l,m,k]))
-                end
+        (;lmax,mmax) = m.spectral_transform
+        for m in 1:mmax+1
+            for l in max(2,m):lmax+1
+                @test -sign(real(vor[l,m])) == sign(real(vor_tend[l,m]))
+                @test -sign(imag(vor[l,m])) == sign(imag(vor_tend[l,m]))
             end
         end
 
-        vor0 = copy(vor_lf1)
-        SpeedyWeather.leapfrog!(vor,vor_tend,m.constants.Δt,m.constants,1)
-        vor1 = view(vor,:,:,2,:)
+        vor0 = copy(vor)
+        vor1 = copy(vor)
+        SpeedyWeather.leapfrog!(vor0,vor1,vor_tend,m.constants.Δt,1,m.constants)
 
         @test any(vor0 .!= vor1)    # check that at least some coefficients are different
         @test any(vor0 .== vor1)    # check that at least some coefficients are identical
 
         # damping should not increase real or imaginary part of variable
-        for lm in eachindex(vor0,vor_lf1)
-            @test real(vor0[lm]) >= real(vor_lf1[lm])
-            @test imag(vor0[lm]) >= imag(vor_lf1[lm])
+        for lm in SpeedyWeather.eachharmonic(vor0,vor)
+            @test abs(real(vor1[lm])) <= abs(real(vor[lm]))
+            @test abs(imag(vor1[lm])) <= abs(imag(vor[lm]))
         end
     end
 end
