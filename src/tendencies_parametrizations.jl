c8163bbe26c3090a0f2fc89bb1bf2e09b304ba6e
Milan <milankloewer@gmx.de>
2022-08-15 10:19:09 +0100

    horizontal loop over parametrizations

--- src/tendencies_parametrizations.jl
+++ src/tendencies_parametrizations.jl
@@ -1,23 +1,40 @@
 """
-Compute physical parametrization tendencies.
+    parametrization_tendencies!(diagn::DiagnosticVariables,
+                                M::PrimitiveEquationModel)
+
+Compute tendencies for u,v,temp,humid from physical parametrizations.
+Extract for each vertical atmospheric column the prognostic variables
+(stored in `diagn` as they are grid-point transformed), loop over all
+grid-points, compute all parametrizations on a single-column basis,
+then write the tendencies back into a horizontal field of tendencies.
 """
-function parametrization_tendencies!(
-    Prog::PrognosticVariables{NF},
-    Diag::DiagnosticVariables{NF},
-    M,
-) where {NF<:AbstractFloat}
-    @unpack pres_grid = Diag.grid_variables
+function parametrization_tendencies!(   diagn::DiagnosticVariables{NF},
+                                        M::PrimitiveEquationModel,
+                                        ) where NF
 
-    # The prognostic variable pres has units of log(hPa), whereas the grid-point pressure
-    # field which we require for the parametrizations has units of hPa, so we take the
-    # exponential here.
-    @. pres_grid = exp(pres_grid)
+    G = M.geometry
+    column = ColumnVariables{NF}(nlev=diagn.nlev)
 
-    get_large_scale_condensation_tendencies!(Diag, M)
+    for ij in eachgridpoint(diagn)
 
-    #Calculate
-    #utend: u-wind tendency (gp)
-    #vtend: v-wind tendency (gp)
-    #ttend: temp. tendency (gp)
-    #htend: spec. hum. tendency (gp)
+        # extract a single atmospheric column for contiguous memory access
+        get_column!(column,diagn,ij,G)
+
+        fill!(column.u_tend,0)      # set tendencies to 0 to accumulate +=
+        fill!(column.v_tend,0)      # parametrizations into
+        fill!(column.temp_tend,0)
+        fill!(column.humid_tend,0)
+
+        # calculate parametrizations
+        # convection!(column, M)
+        large_scale_condensation!(column, M)
+        # clouds!(column, M)
+        # shortwave_radiation!(column, M)
+        # longwave_radiation!(column, M)
+        # surface_fluxes!(column,M)
+        # vertical_diffusion!(column,M)
+
+        # write tendencies from parametrizations back into horizontal fields
+        write_column_tendencies!(diagn,column,ij)
+    end
 end
