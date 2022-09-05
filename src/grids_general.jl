50d47af775fca6594085d9bc890409b4aecfcbb1
Milan Klöwer <milankloewer@gmx.de>
2022-09-05 18:23:32 +0100

    grid requirements as docstring

--- src/grids_general.jl
+++ src/grids_general.jl
@@ -1,7 +1,3 @@
-"""
-    abstract type AbstractGrid{T} <: AbstractVector{T} end
-
-The abstract supertype for all spatial grids on the sphere supported by SpeedyWeather.jl."""
 abstract type AbstractGrid{T} <: AbstractVector{T} end
 
 # all AbstractGrids have their grid points stored in a vector field `data`
@@ -20,57 +16,62 @@ end
 @inline Base.getindex(G::AbstractGrid,r::AbstractRange) = G.data[r]
 @inline Base.setindex!(G::AbstractGrid,x::AbstractVector,r::AbstractRange) = setindex!(G.data,x,r)
 
+"""
+    abstract type AbstractGrid{T} <: AbstractVector{T} end
 
-# List all the methods required to be implemented for any Grid types here
-# and define the  default behaviour as raising errors
-# This would clarify what interface we need to implement when introducing a new Grid
+The abstract supertype for all spatial grids on the sphere supported by SpeedyWeather.jl.
+Every new grid has to be of the form
 
-truncation_order(::Type{G}) where {G<:AbstractGrid} =
-    error("truncation_order(::Type{G}) not implemented for G==$G")
-get_truncation(::Type{G},n::Integer) where {G<:AbstractGrid} =
-    error("get_truncation(::Type{G},n::Integer) not implemented for G==$G")
-get_resolution(::Type{G},trunc::Integer) where {G<:AbstractGrid} =
-    error("get_resolution(::Type{G},trunc::Integer) not implemented for G==$G")
-# define nlat_half for all grids (HEALPixGrid is different as it doesn't use nlat_half as resolution parameter)
-get_nresolution(G::AbstractGrid) =
-    error("get_nresolution(G) not implemented for G==$G")
-# define whether there's an odd number of latitude rings for a grid
-nlat_odd(::Type{G}) where {G<:AbstractGrid} =
-    error("nlat_odd(::Type{G}) not implemented for G==$G")
-# return the maxmimum number of longitude points for a grid and its resolution parameter nlat_half/nside
-get_nlon(::Type{G},nresolution::Integer) where {G<:AbstractGrid} =
-    error("get_nlon(::Type{G},nresolution::Integer) not implemented for G==$G")
-# get the number of longitude points at given latitude ring j number 1...J from north to south
-get_nlon_per_ring(::Type{G},nresolution::Integer,j::Integer) where {G<:AbstractGrid} =
-    error("get_nlon_per_ring(::Type{G},nresolution::Integer,j::Integer) not implemented for G==$G")
-# total number of grid points per grid type
-get_npoints(::Type{G},nresolution::Integer) where {G<:AbstractGrid} =
-    error("get_npoints(::Type{G},nresolution::Integer) not implemented for G==$G")
-# colatitude [radians] vectors
-get_colat(::Type{G},nresolution::Integer) where {G<:AbstractGrid} =
-    error("get_colat(::Type{G},nresolution::Integer) not implemented for G==$G")
-# lon [radians] vectors for full grids (empty vectors otherwise)
-get_lon(::Type{G},nresolution::Integer) where {G<:AbstractGrid} =
-    error("get_lon(::Type{G},nresolution::Integer) not implemented for G==$G")
-# get coordinates
-get_colatlons(::Type{G},nresolution::Integer) where {G<:AbstractGrid} =
-    error("get_colatlons(::Type{G},nresolution::Integer) not implemented for G==$G")
-each_index_in_ring(::Type{G},j::Integer,nresolution::Integer) where {G<:AbstractGrid} =
-    error("each+index_in_ring(::Type{G},j::Integer,nresolution::Integer) not implemented for G==$G")
-get_quadrature_weights(::Type{G},nresolution::Integer) where {G<:AbstractGrid} =
-    error("get_quadrature_weights(::Type{G},nresolution::Integer) not implemented for G==$G")
+    abstract type AbstractGridClass{T} <: AbstractGrid{T} end
+    struct MyNewGrid{T} <: AbstractGridClass{T}
+        data::Vector{T}     # all grid points unravelled into a vector
+        n::Int              # resolution parameter, like `nlat_half`, `nside` etc
+    end
+    
+`MyNewGrid` should belong to a grid class like `AbstractFullGrid`, `AbstractOctahedralGrid` or
+`AbstractHEALPixGrid` (that already exist but you may introduce a new class of grids) that share
+certain features such as the number of longitude points per latitude ring and indexing, but may
+have different latitudes or offset rotations. Each new grid `Grid` (or grid class) then has to
+implement the following methods (see grids.jl)
+    
+Fundamental grid properties
+    get_nresolution     # returns the resolution parameter nlat_half, nside, etc.
+    get_npoints         # total number of grid points
+    nlat_odd            # does the grid have an odd number of latitude rings?
+    get_nlat            # total number of latitude rings
+    get_nlat_half       # number of latitude rings on one hemisphere incl Equator
+    
+Indexing
+    get_nlon_max        # maximum number of longitudes points (at the Equator)
+    get_nlon_per_ring   # number of longitudes on ring j
+    each_index_in_ring  # a unit range that indexes all longitude points on a ring
+    
+Coordinates
+    get_colat           # vector of colatitudes (radians)
+    get_colatlon        # vectors of colatitudes, longitudes (both radians)
+
+Spectral truncation
+    truncation_order    # linear, quadratic, cubic = 1,2,3 for grid 
+    get_truncation      # spectral truncation given a grid resolution
+    get_resolution      # grid resolution given a spectral truncation
+
+Quadrature weights and solid angles
+    get_quadrature_weights  # = sinθ Δθ for grid points on ring j for meridional integration
+    get_solid_angle         # = sinθ Δθ Δϕ, solid angle of grid points on ring j
+"""
+AbstractGrid
 
 # Define methods that are universally applicable to any G<:AbstractGrid here
 # generator functions for grid
-Base.zeros(::Type{G},n::Integer) where {G<:AbstractGrid{T}} where T =
-    G(zeros(T,get_npoints(G,n)),n)
+Base.zeros(::Type{Grid},n::Integer) where {Grid<:AbstractGrid{T}} where T =
+    Grid(zeros(T,get_npoints(Grid,n)),n)
 # use Float64 if not provided
-Base.zeros(::Type{G},n::Integer) where {G<:AbstractGrid} = zeros(G{Float64},n)
-# zero element of an AbstractGrid instance G by packing a zero(::Vector) into G
-Base.zero(g::G) where {G<:AbstractGrid} = G(zero(g.data))
+Base.zeros(::Type{Grid},n::Integer) where {Grid<:AbstractGrid} = zeros(Grid{Float64},n)
+# zero element of an AbstractGrid instance grid by packing a zero(::Vector) into grid
+Base.zero(grid::Grid) where {Grid<:AbstractGrid} = Grid(zero(grid.data))
 
-get_truncation(grid::G) where {G<:AbstractGrid} = get_truncation(G,get_nresolution(grid))
-get_nlat_half(::G,n::Integer) where {G<:AbstractGrid} = get_nlat_half(G,n) 
+get_truncation(grid::Grid) where {Grid<:AbstractGrid} = get_truncation(Grid,get_nresolution(grid))
+get_nlat_half(::Grid,n::Integer) where {Grid<:AbstractGrid} = get_nlat_half(Grid,n) 
 nlat_odd(grid::AbstractGrid) = nlat_odd(typeof(grid))
 get_nlat_half(::Type{<:AbstractGrid},nlat_half::Integer) = nlat_half
 get_nlat(Grid::Type{<:AbstractGrid},n::Integer) = 2get_nlat_half(Grid,n) - nlat_odd(Grid)
@@ -92,4 +93,17 @@ function eachring(grids::Grid...) where {Grid<:AbstractGrid}
         @boundscheck length(grid) == length(grids[1]) || throw(BoundsError)
     end
     return eachring(grids[1])
+end
+
+"""
+    nlons = get_nlons(  Grid::Type{<:AbstractGrid},
+                        nresolution::Integer;
+                        both_hemispheres::Bool=false)
+
+Returns a vector `nlons` for the number of longitude points per latitude ring, north to south.
+Provide grid `Grid` and its resolution parameter `nresolution`. For both_hemisphere==false only
+the northern hemisphere (incl Equator) is returned."""
+function get_nlons(Grid::Type{<:AbstractGrid},nresolution::Integer;both_hemispheres::Bool=false)
+    n = both_hemispheres ? get_nlat(Grid,nresolution) : get_nlat_half(Grid,nresolution)
+    return [get_nlon_per_ring(Grid,nresolution,j) for j in 1:n]
 end
