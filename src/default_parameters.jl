3321564f8410ae863aee0fe956b9be74972e3dbf
Milan <milankloewer@gmx.de>
2022-09-12 18:28:29 +0100

    outputgrid parameters added

--- src/default_parameters.jl
+++ src/default_parameters.jl
@@ -8,14 +8,14 @@ The default values of the keywords define the default model setup.
 @with_kw struct Parameters
 
     # NUMBER FORMATS
-    NF::DataType                        # number format (default is defined in run_speedy.jl)
+    NF::DataType                        # number format (default defined in run_speedy)
 
     # MODEL
-    model::Symbol=:barotropic           # :barotropic, :shallowwater, or :primitive
+    model::Symbol = :barotropic         # :barotropic, :shallowwater, or :primitive
 
     # RESOLUTION AND GRID
-    trunc::Int=31                                   # spectral truncation
-    Grid::Type{<:AbstractGrid}=FullGaussianGrid     # define the grid type
+    trunc::Int = 31                                 # spectral truncation
+    Grid::Type{<:AbstractGrid} = FullGaussianGrid   # define the grid type
     
     # PHYSICAL CONSTANTS
     radius_earth::Real=6.371e6          # radius of Earth [m]
@@ -46,9 +46,9 @@ The default values of the keywords define the default model setup.
     # of the nlev vertical levels, defined by a generalised logistic function,
     # interpolating ECMWF's L31 configuration
     GLcoefs::GenLogisticCoefs = GenLogisticCoefs{NF}()
-    n_stratosphere_levels::Int=2                    # number of vertical levels used for the stratosphere
-    σ_levels_half::Vector{Real}=Vector{Real}()      # vector of σ half levels, only if set manually, otherwise an empty vector
-    nlev::Int=nlev_default(model, σ_levels_half)    # number of vertical levels 
+    n_stratosphere_levels::Integer=2                    # number of vertical levels used for the stratosphere
+    σ_levels_half::Vector{Real}=[]                      # vector of σ half levels, only if set manually, otherwise an empty vector
+    nlev::Integer=nlev_default(model, σ_levels_half)    # number of vertical levels 
 
     # DIFFUSION AND DRAG
     diffusion_power::Real=4                 # Power n of Laplacian in horizontal diffusion ∇²ⁿ
@@ -58,9 +58,9 @@ The default values of the keywords define the default model setup.
     damping_time_strat::Real=24*30          # Damping time [hrs] for drag on zonal-mean wind in the stratosphere
 
     # PARAMETRIZATIONS
-    seasonal_cycle::Bool=true                  # Seasonal cycle?
-    n_shortwave::Int=3                         # Compute shortwave radiation every n steps
-    sppt_on::Bool=false                        # Turn on SPPT?
+    seasonal_cycle::Bool=true               # Seasonal cycle?
+    n_shortwave::Integer=3                  # Compute shortwave radiation every n steps
+    sppt_on::Bool=false                     # Turn on SPPT?
     magnus_coefs::MagnusCoefs = MagnusCoefs{NF}()  # For computing saturation vapour pressure
 
     # Large-Scale Condensation (from table B10)
@@ -71,50 +71,69 @@ The default values of the keywords define the default model setup.
     humid_relax_time_lsc::Real = 4.0  # Relaxation time for humidity (hours)
 
     # Convection
-    pres_thresh_cnv::Real = 0.8            # Minimum (normalised) surface pressure for the occurrence of convection
-    RH_thresh_pbl_cnv::Real = 0.9          # Relative humidity threshold for convection in PBL
-    RH_thresh_trop_cnv::Real = 0.7         # Relative humidity threshold for convection in the troposphere
-    humid_relax_time_cnv::Real = 6.0       # Relaxation time for PBL humidity (hours)
-    max_entrainment::Real = 0.5            # Maximum entrainment as a fraction of cloud-base mass flux
-    ratio_secondary_mass_flux::Real = 0.8  # Ratio between secondary and primary mass flux at cloud-base
+    pres_thresh_cnv::Real = 0.8             # Minimum (normalised) surface pressure for the occurrence of convection
+    RH_thresh_pbl_cnv::Real = 0.9           # Relative humidity threshold for convection in PBL
+    RH_thresh_trop_cnv::Real = 0.7          # Relative humidity threshold for convection in the troposphere
+    humid_relax_time_cnv::Real = 6.0        # Relaxation time for PBL humidity (hours)
+    max_entrainment::Real = 0.5             # Maximum entrainment as a fraction of cloud-base mass flux
+    ratio_secondary_mass_flux::Real = 0.8   # Ratio between secondary and primary mass flux at cloud-base
 
     # TIME STEPPING
-    Δt_at_T85::Real=20                  # time step in minutes for T85, scale linearly to trunc
-    n_days::Real=10                     # number of days to integrate for
+    Δt_at_T31::Real = 60                # time step in minutes for T31, scale linearly to trunc
+    n_days::Real = 10                   # number of days to integrate for
 
     # NUMERICS
-    robert_filter::Real=0.05            # Robert (1966) time filter coefficeint to suppress comput. mode
-    williams_filter::Real=0.53          # William's time filter (Amezcua 2011) coefficient for 3rd order acc
-    implicit_α::Real=0.5                # coefficient for semi-implicit computations to filter gravity waves
-    recompute_legendre::Bool=false      # recomputation is slower but requires less memory
+    robert_filter::Real = 0.05          # Robert (1966) time filter coefficeint to suppress comput. mode
+    williams_filter::Real = 0.53        # William's time filter (Amezcua 2011) coefficient for 3rd order acc
+    implicit_α::Real = 0.5              # coefficient for semi-implicit computations to filter gravity waves
+    
+    # LEGENDRE TRANSFORM AND POLYNOMIALS
+    recompute_legendre::Bool = false    # recomputation is slower but requires less memory
+    legendre_NF::DataType = Float64     # which format to use to calculate the Legendre polynomials
+    legendre_shortcut::Symbol = :linear
 
     # BOUNDARY FILES
-    boundary_path::String=""        # package location is default
-    orography_path::String=boundary_path
-    orography_file::String="orography_F512.nc"
+    boundary_path::String = ""          # package location is default
+    orography_path::String = boundary_path
+    orography_file::String = "orography_F512.nc"
 
     # INITIAL CONDITIONS
-    seed::Int=abs(rand(Int))            # a random seed that's used in initialize_speedy for the global RNG
+    seed::Int = abs(rand(Int))          # a random seed that's used in initialize_speedy for the global RNG
     initial_conditions::Symbol=:barotropic_vorticity    # :rest, :barotropic_vorticity or :restart
 
     # OUTPUT
-    verbose::Bool=true              # print dialog for feedback
-    output::Bool=false              # Store data in netCDF?
-    output_dt::Real=6               # output time step [hours]
-    output_startdate::DateTime=DateTime(2000,1,1)
-    out_path::String=pwd()          # path to output folder
-    output_vars::Vector{String}=["u","v","temp","humid","pres"]
-    compression_level::Int=3        # 1=low but fast, 9=high but slow
-    keepbits::Int=7                 # mantissa bits to keep for every variable
+    verbose::Bool = true            # print dialog for feedback
+    output::Bool = false            # Store data in netCDF?
+    output_dt::Real = 6             # output time step [hours]
+    output_startdate::DateTime = DateTime(2000,1,1)
+    output_path::String = pwd()     # path to output folder
+    output_filename::String="output.nc"     # name of the output netcdf file
+    output_vars::Vector{Symbol}=[:vor]      # variables to output: :u, :v, :vor, :div, :
+    compression_level::Integer = 3  # 1=low but fast, 9=high but slow
+    keepbits::Integer = 7           # mantissa bits to keep for every variable
     version::VersionNumber=pkgversion(SpeedyWeather)
 
+    # OUTPUT GRID
+    output_NF::DataType = NF        # number format used for output
+    missing_value::Real = -999999   # missing value to be used in netcdf output
+    output_grid::Symbol=:full       # :full, pick the corresponding full grid for reduced grids
+                                    # or :matrix, sort gridpoints into a matrix
+    output_quadrant_rotation::NTuple{4,Integer}=(0,1,2,3)
+    output_matrix_quadrant::NTuple{4,Tuple{Integer,Integer}}=((2,2),(1,2),(1,1),(2,1))
+
     # RESTART
-    write_restart::Bool=output      # also write restart file if output==true?
-    restart_path::String=out_path   # path for restart file
-    restart_id::Integer=1           # run_id of restart file in run????/restart.jld2
+    write_restart::Bool = output        # also write restart file if output==true?
+    restart_path::String = output_path  # path for restart file
+    restart_id::Integer = 1             # run_id of restart file in run????/restart.jld2
 end
 
-function nlev_default(model::Symbol, σ_levels_half::Vector{T}) where T<:Real
+"""
+    nlev = nlev_default(model::Symbol, σ_levels_half::AbstractVector)
+
+Number of vertical levels chosen either automatically based on `model`,
+or from the length of `σ_levels_half` if not a 0-length vector
+(default if not specified parameter)."""
+function nlev_default(model::Symbol, σ_levels_half::AbstractVector)
     if length(σ_levels_half) == 0   # choose nlev automatically 
         model == :barotropic && return 1
         model == :shallowwater && return 1
