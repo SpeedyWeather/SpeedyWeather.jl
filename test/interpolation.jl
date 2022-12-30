089dc25c8e70325800979c85da5fc10fb3a4d1eb
Milan <milankloewer@gmx.de>
2022-12-30 18:16:12 -0500

    Test: interpolate zonally-constant field

--- test/interpolation.jl
+++ test/interpolation.jl
@@ -24,51 +24,52 @@
     end
 end
 
-# @testset "Interpolate zonally-constant field" begin
-#     npoints = 4
+@testset "Interpolate zonally-constant field" begin
+    npoints = 10000
     
-#     for Grid in (   FullGaussianGrid,
-#                     OctahedralGaussianGrid,
-#                     OctahedralClenshawGrid,
-#                     HEALPixGrid,
-#                     HEALPix4Grid)
+    @testset for Grid in (   FullGaussianGrid,
+                    OctahedralGaussianGrid,
+                    OctahedralClenshawGrid,
+                    HEALPixGrid,
+                    HEALPix4Grid)
         
-#         for NF in (Float32,Float64)
+        @testset for NF in (Float32,Float64)
         
-#             A = zeros(Grid{NF},8)           # something small
-#             G = SpeedyWeather.GridGeometry(A)
+            A = zeros(Grid{NF},32)          # some resolution
+            G = SpeedyWeather.GridGeometry(A)
+            lat1 = G.latd[2]                # latitude of first ring
+            println(lat1)
             
-#             for (j,ring) in enumerate(SpeedyWeather.eachring(A))
-#                 θ = G.latd[j+1]     # G.latd also includes 90˚N
-#                 for ij in ring
-#                     A[ij] = θ
-#                 end
-#             end
+            for (j,ring) in enumerate(SpeedyWeather.eachring(A))
+                θ = G.latd[j+1]     # G.latd also includes 90˚N
+                for ij in ring
+                    A[ij] = θ
+                end
+            end
 
-#             θs = 180*rand(npoints) .- 90    # some latitudes in [-90˚,90˚N]
-#             λs = 360*rand(npoints)          # some longitudes in [0˚,360˚E]
+            # don't interpolate above first or below last ring
+            # northpole value isn't 90 as it's just the average of the
+            # first ring, same for south pole
+            θs = 2lat1*rand(npoints) .- lat1    # some latitudes in [-90˚,90˚N]
+            λs = 360*rand(npoints)          # some longitudes in [0˚,360˚E]
             
-#             println(θs)
-#             As = SpeedyWeather.interpolate(θs,λs,A)
-#             println(As)
-#             println("---")
+            As = SpeedyWeather.interpolate(θs,λs,A)
 
-
-#             # for (a,θ) in (As,θs)
-#             #     @test a ≈ θ
-#             # end
-#         end
-#     end
-# end
+            for (a,θ) in zip(As,θs)
+                @test a ≈ θ
+            end
+        end
+    end
+end
 
 @testset "Find latitude rings and weights" begin
-    for Grid in (   FullGaussianGrid,
+    @testset for Grid in (   FullGaussianGrid,
                     OctahedralGaussianGrid,
                     OctahedralClenshawGrid,
                     HEALPixGrid,
                     HEALPix4Grid)
 
-        for nlat_half in [4,8,16] 
+        @testset for nlat_half in [4,8,16] 
             G = SpeedyWeather.GridGeometry(Grid,nlat_half)
             latd = G.latd
 
