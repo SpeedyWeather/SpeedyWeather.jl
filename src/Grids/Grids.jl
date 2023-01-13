3f0aace7292afe749a5b4b5446504556116ec694
Milan <milankloewer@gmx.de>
2023-01-13 14:43:19 -0500

    +interp method, show ring info for grids

--- src/Grids/Grids.jl
+++ src/Grids/Grids.jl
@@ -1,6 +1,7 @@
 #TODO turn into module?
 
 include("grids_general.jl") # def AbstractGrid
+include("show.jl")
 
 include("full_grids.jl")    # def AbstractFullGrid, Full*Grid, * = Gaussian, Clenshaw, HEALPix, HEALPix4
 include("octahedral.jl")    # def AbstractOctahedralGrid, OctahedralGaussianGrid, OctahedralClenshawGrid
