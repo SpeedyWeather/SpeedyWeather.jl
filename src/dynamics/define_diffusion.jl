bd377de1e599e66f69e0ac595f15b51277d4620b
Milan <milankloewer@gmx.de>
2023-04-07 17:48:07 -0400

    Diffusion tapering for stratosphere

--- src/dynamics/define_diffusion.jl
+++ src/dynamics/define_diffusion.jl
@@ -6,9 +6,10 @@ Base.@kwdef struct HyperDiffusion <: DiffusionParameters
     # additional diffusion in stratosphere
     power_stratosphere::Int = 1                 # additional ∇² for stratosphere
     time_scale_stratosphere::Float64 = 12       # associated time scale
+    tapering_σ::Float64 = 0.2                   # increase 1/time scale linearly above this σ
 
     # reduce time scale of diffusion linearly with increasing model resolution?
-    scale_with_resolution::Bool = true      # e.g. T31 = 2.5hrs diffusion time scale, T63 = 1.25 hrs
+    scale_with_resolution::Bool = true          # e.g. 2.5hrs for T31, 1.25 hrs for T63 etc
 end
 
 """ 
@@ -18,13 +19,10 @@ Horizontal Diffusion struct containing all the preallocated arrays for the calcu
 of horizontal diffusion."""
 struct HorizontalDiffusion{NF<:AbstractFloat}   # Number format NF
     
-    # Explicit part of the (hyper) diffusion, precalculated for each spherical harm degree
-    ∇²ⁿ::Vector{NF}                             # everywhere
-    ∇²ⁿ_stratosphere::Vector{NF}                # +extra diffusion in the stratosphere
-    
-    # Implicit part
-    ∇²ⁿ_implicit::Vector{NF}
-    ∇²ⁿ_implicit_stratosphere::Vector{NF}
+    # (Hyper) diffusion, precalculated for each spherical harm degree
+    # and for each layer (to allow for varying orders/strength in the vertical)
+    ∇²ⁿ::Vector{Vector{NF}}                     # explicit part
+    ∇²ⁿ_implicit::Vector{Vector{NF}}            # implicit part
 end
 
 """
@@ -38,11 +36,13 @@ temperature and humidity.
 function HorizontalDiffusion(   scheme::HyperDiffusion,
                                 P::Parameters,
                                 C::DynamicsConstants,
+                                G::Geometry,
                                 S::SpectralTransform{NF}) where NF
     @unpack lmax,mmax = S
     @unpack radius = P.planet
-    @unpack power, power_stratosphere = scheme
+    @unpack power, power_stratosphere, tapering_σ = scheme
     @unpack Δt = C
+    @unpack nlev, σ_levels_full = G
 
     # reuce diffusion time scale (=increase diffusion) with resolution?
     if scheme.scale_with_resolution
@@ -53,36 +53,35 @@ function HorizontalDiffusion(   scheme::HyperDiffusion,
         @unpack time_scale, time_scale_stratosphere = scheme
     end
 
-    # Diffusion is applied by multiplication of the (absolute) eigenvalues of the Laplacian l*(l+1)
-    # normalise by the largest eigenvalue lmax*(lmax+1) such that the highest wavenumber lmax
+    # Diffusion is applied by multiplication of the eigenvalues of the Laplacian -l*(l+1)
+    # normalise by the largest eigenvalue -lmax*(lmax+1) such that the highest wavenumber lmax
     # is dampened to 0 at the given time scale raise to a power of the Laplacian for hyperdiffusion
     # (=more scale-selective for smaller wavenumbers)
-    largest_eigenvalue = lmax*(lmax+1)
+    largest_eigenvalue = -lmax*(lmax+1)
 
     # PREALLOCATE as vector as only dependend on degree l
     # Damping coefficients for explicit part of the diffusion (=ν∇²ⁿ)
-    ∇²ⁿ = zeros(NF,lmax+2)                  # for temperature and vorticity (explicit)
-    ∇²ⁿ_stratosphere = zeros(NF,lmax+2)     # for divergence (explicit)
+    ∇²ⁿ = [zeros(NF,lmax+2) for _ in 1:nlev]                # for temp, vor, div (explicit)
+    ∇²ⁿ_implicit = [zeros(NF,lmax+2) for _ in 1:nlev]       # Implicit part (= 1/(1+2Δtν∇²ⁿ))
 
-    # Implicit part (= 1/(1+2Δtν∇²ⁿ))
-    ∇²ⁿ_implicit = zeros(NF,lmax+2)
-    ∇²ⁿ_implicit_stratosphere = zeros(NF,lmax+2)
+    for k in 1:nlev         # every layer is a combination of two Laplacians of different orders
 
-    # PRECALCULATE for every degree l
-    for l in 0:lmax+1
-        # eigenvalue is l*(l+1), but 1-based here l→l-1
-        norm_eigenvalue = l*(l+1)/largest_eigenvalue        # normal diffusion ∇²
+        # tapering: go from 1 to 0 between σ=0 and tapering_σ and no additional diffusion below
+        σ = σ_levels_full[k]
+        tapering = max(0,(tapering_σ-σ)/tapering_σ)         # ∈ [0,1]
+        
+        for l in 0:lmax+1   # PRECALCULATE for every degree l, but indendent of order m
+            eigenvalue_norm = -l*(l+1)/largest_eigenvalue   # normalised diffusion ∇², power=1
 
-        # Explicit part (=ν∇²ⁿ), time scales to damping frequencies [1/s] times norm. eigenvalue
-        # time scale [hrs] *3600-> [s]
-        ∇²ⁿ[l+1] = norm_eigenvalue^power/(3600*time_scale)*radius
-        ∇²ⁿ_implicit[l+1] = 1/(1+2Δt*∇²ⁿ[l+1])              # and implicit part of the diffusion (= 1/(1+2Δtν∇²ⁿ))
-
-        # add additional diffusion for stratosphere
-        ∇²ⁿ_stratosphere[l+1] = ∇²ⁿ[l+1] + 
-            norm_eigenvalue^power_stratosphere/(3600*time_scale_stratosphere)*radius
-        ∇²ⁿ_implicit_stratosphere[l+1] = 1/(1+2Δt*∇²ⁿ_stratosphere[l+1])
+            # Explicit part (=-ν∇²ⁿ), time scales to damping frequencies [1/s] times norm. eigenvalue
+            # time scale [hrs] *3600-> [s] and add additional diffusion for stratosphere
+            ∇²ⁿ[k][l+1] = -eigenvalue_norm^power/(3600*time_scale)*radius +
+                -tapering*eigenvalue_norm^power_stratosphere/(3600*time_scale_stratosphere)*radius
+            
+            # and implicit part of the diffusion (= 1/(1-2Δtν∇²ⁿ))
+            ∇²ⁿ_implicit[k][l+1] = 1/(1-2Δt*∇²ⁿ[k][l+1])           
+        end
     end
 
-    return HorizontalDiffusion(∇²ⁿ,∇²ⁿ_stratosphere,∇²ⁿ_implicit,∇²ⁿ_implicit_stratosphere)
+    return HorizontalDiffusion(∇²ⁿ,∇²ⁿ_implicit)
 end
--- src/dynamics/diffusion.jl
+++ src/dynamics/diffusion.jl
@@ -21,7 +21,7 @@ function horizontal_diffusion!( tendency::LowerTriangularMatrix{Complex{NF}},   
     for m in 1:mmax         # loops over all columns/order m
         for l in m:lmax-1   # but skips the lmax+2 degree (1-based)
             lm += 1         # single index lm corresponding to harmonic l,m
-            tendency[lm] = (tendency[lm] - ∇²ⁿ_expl[l]*A[lm])*∇²ⁿ_impl[l]
+            tendency[lm] = (tendency[lm] + ∇²ⁿ_expl[l]*A[lm])*∇²ⁿ_impl[l]
         end
         lm += 1         # skip last row, LowerTriangularMatrices are of size lmax+2 x mmax+1
     end
@@ -35,17 +35,11 @@ diffusion_vars(::PrimitiveEquation) = (:vor,:div,:temp)
 function horizontal_diffusion!( progn::PrognosticVariablesLeapfrog,
                                 diagn::DiagnosticVariablesLayer,
                                 model::ModelSetup,
-                                lf::Int=1)                          # leapfrog index used (2 is unstable)
+                                lf::Int=1)      # leapfrog index used (2 is unstable)
 
-    k = diagn.k
-    @unpack n_stratosphere_levels = model.geometry
-
-    if k <= n_stratosphere_levels   # if level in the stratosphere use stratospheric diffusion operators
-        ∇²ⁿ = model.horizontal_diffusion.∇²ⁿ_stratosphere
-        ∇²ⁿ_implicit = model.horizontal_diffusion.∇²ⁿ_implicit_stratosphere
-    else
-        @unpack ∇²ⁿ, ∇²ⁿ_implicit = model.horizontal_diffusion
-    end
+    k = diagn.k     # pick precalculated hyperdiffusion operator for layer k
+    ∇²ⁿ = model.horizontal_diffusion.∇²ⁿ[k]
+    ∇²ⁿ_implicit = model.horizontal_diffusion.∇²ⁿ_implicit[k]
 
     for varname in diffusion_vars(model)
         var = getfield(progn.leapfrog[lf],varname)
