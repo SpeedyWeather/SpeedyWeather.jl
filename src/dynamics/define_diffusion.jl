99003bdd84f79fec07f91bd2ab0cccf483d41b67
Milan <milankloewer@gmx.de>
2023-04-17 08:48:53 -0400

    HyperDiffusion: add several powers, time_scales

--- src/dynamics/define_diffusion.jl
+++ src/dynamics/define_diffusion.jl
@@ -1,14 +1,11 @@
 Base.@kwdef struct HyperDiffusion <: DiffusionParameters
     # hyperdiffusion for temp, vor, div everywhere
-    power::Int = 4                              # Power n of Laplacian in horizontal diffusion ∇²ⁿ
-    time_scale::Float64 = 2.5                   # Diffusion time scale [hrs] for temp, vor, div
-    
-    # additional diffusion in stratosphere
-    power_stratosphere::Int = 2                 # different power for stratosphere
-    tapering_σ::Float64 = 0.2                   # scale towards that power linearly above this σ
-
-    # reduce time scale of diffusion linearly with increasing model resolution?
-    scale_with_resolution::Bool = true          # e.g. 2.5hrs for T31, 1.25 hrs for T63 etc
+    # several powers of Laplacians, default 4 and 2, are added
+    # with respective time scales and scalings with resolution
+    powers::Vector{Float64} = [4.0,2.0]                 # Powers of Laplacians
+    time_scales::Vector{Float64} = [2.4,24.0]           # Diffusion time scales [hrs]
+    resolution_scalings::Vector{Float64} = [1.0,2.0]    # 1: (inverse) linear with T
+                                                        # 2: (inverse) quadratic, etc
 end
 
 """ 
@@ -39,17 +36,15 @@ function HorizontalDiffusion(   scheme::HyperDiffusion,
                                 S::SpectralTransform{NF}) where NF
     @unpack lmax,mmax = S
     @unpack radius = P.planet
-    @unpack power, power_stratosphere, tapering_σ = scheme
+    @unpack powers, resolution_scalings = scheme
     @unpack Δt = C
-    @unpack nlev, σ_levels_full = G
+    @unpack nlev = G
 
-    # reuce diffusion time scale (=increase diffusion) with resolution?
-    if scheme.scale_with_resolution
-        # use values in scheme for T31 (=32 here) and decrease linearly with lmax+1
-        time_scale = scheme.time_scale * (32/(lmax+1))
-    else
-        # @unpack time_scale, time_scale_stratosphere = scheme
-        @unpack time_scale = scheme
+    # Reduce diffusion time scale (=increase diffusion) with resolution
+    time_scales = zero(scheme.time_scales)
+    for i in eachindex(time_scales,resolution_scalings)
+        # use values in scheme for T31 (=32 here) and decrease with lmax+1
+        time_scales[i] = scheme.time_scales[i] * (32/lmax+1)^resolution_scalings[i]
     end
 
     # Diffusion is applied by multiplication of the eigenvalues of the Laplacian -l*(l+1)
@@ -63,19 +58,18 @@ function HorizontalDiffusion(   scheme::HyperDiffusion,
     ∇²ⁿ = [zeros(NF,lmax+2) for _ in 1:nlev]                # for temp, vor, div (explicit)
     ∇²ⁿ_implicit = [zeros(NF,lmax+2) for _ in 1:nlev]       # Implicit part (= 1/(1+2Δtν∇²ⁿ))
 
-    for k in 1:nlev         # every layer is a combination of two Laplacians of different orders
-
-        # tapering: go from 1 to 0 between σ=0 and tapering_σ
-        σ = σ_levels_full[k]
-        tapering = max(0,(tapering_σ-σ)/tapering_σ)         # ∈ [0,1]
-        p = power + tapering*(power_stratosphere - power)     
-        
+    for k in 1:nlev         # every layer is a combination of Laplacians of different orders
         for l in 0:lmax+1   # PRECALCULATE for every degree l, but indendent of order m
             eigenvalue_norm = -l*(l+1)/largest_eigenvalue   # normalised diffusion ∇², power=1
 
             # Explicit part (=-ν∇²ⁿ), time scales to damping frequencies [1/s] times norm. eigenvalue
-            # time scale [hrs] *3600-> [s] and add additional diffusion for stratosphere
-            ∇²ⁿ[k][l+1] = -eigenvalue_norm^p/(3600*time_scale)*radius
+            # time scale [hrs] *3600-> [s]
+            ∇²ⁿ[k][l+1] = 0
+            for i in eachindex(powers,time_scales)
+                power = powers[i]
+                time_scale = time_scales[i]
+                ∇²ⁿ[k][l+1] += -eigenvalue_norm^power/(3600*time_scale)*radius
+            end
             
             # and implicit part of the diffusion (= 1/(1-2Δtν∇²ⁿ))
             ∇²ⁿ_implicit[k][l+1] = 1/(1-2Δt*∇²ⁿ[k][l+1])           
