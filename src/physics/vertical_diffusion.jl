a0f010de0f952d6ffcd63bf53fabcc825eb27f83
Milan <milankloewer@gmx.de>
2023-04-19 23:51:38 -0400

    VerticalDiffusion: Float64 defaults

--- src/physics/vertical_diffusion.jl
+++ src/physics/vertical_diffusion.jl
@@ -14,9 +14,9 @@ function initialize_vertical_diffusion!(K::ParameterizationConstants,
 end 
 
 Base.@kwdef struct VerticalLaplacian{NF<:Real} <: VerticalDiffusion{NF}
-    time_scale::NF = 1  # [hours] time scale for slow global relaxation
-    umax::NF = 80       # velocity scale [m/s] to scale the diffusion with |u|/umax
-    ΔTmax::NF = 50      # temperature gradient scale [K] (divided by Δσ)
+    time_scale::NF = 1.0    # [hours] time scale for slow global relaxation
+    umax::NF = 80.0         # velocity scale [m/s] to scale the diffusion with |u|/umax
+    ΔTmax::NF = 50.0        # temperature gradient scale [K] (divided by Δσ)
 
     scale_with_temperature_gradient::Bool = true
     scale_with_speed::Bool = false
@@ -38,7 +38,7 @@ function vertical_diffusion!(   column::ColumnVariables,
     ∂σ = model.parameterization_constants.vert_diff_∂σ
 
     # GET DIFFUSION COEFFICIENT as a function of u,v,temp and surface pressure
-    ν .= model.geometry.radius*inv(scheme.time_scale*3600)    # [hrs] → [s]
+    ν .= model.geometry.radius*inv(scheme.time_scale*3600)    # *3600 for [hrs] → [s]
     scheme.pressure_coordinates && ν .*= (model.parameters.pres_ref*100/column.pres[end])^2
     if scheme.scale_with_temperature_gradient
         @inbounds for k in 1:nlev-1
@@ -50,26 +50,26 @@ function vertical_diffusion!(   column::ColumnVariables,
     # DO DIFFUSION
     # top layer with no flux boundary conditions at k=1/2
     @inbounds begin
-    ν∇²_below = ν[1]*∇²_below[1]
-    u_tend[1] += ν∇²_below*(u[2] - u[1])
-    v_tend[1] += ν∇²_below*(v[2] - v[1])
-    temp_tend[1] += ν∇²_below*(temp[2] - temp[1])
+        ν∇²_below = ν[1]*∇²_below[1]
+        u_tend[1] += ν∇²_below*(u[2] - u[1])
+        v_tend[1] += ν∇²_below*(v[2] - v[1])
+        temp_tend[1] += ν∇²_below*(temp[2] - temp[1])
 
-    # full Laplacian in other layers
-    for k in 2:nlev-1
-        ν∇²_above = ν[k-1]*∇²_above[k]
-        ν∇²_below = ν[k]*∇²_below[k]
-        ν∇²_at_k = ν∇²_above + ν∇²_below
-        u_tend[k] += ν∇²_below*u[k+1] - ν∇²_at_k*u[k] + ν∇²_above*u[k-1]
-        v_tend[k] += ν∇²_below*v[k+1] - ν∇²_at_k*v[k] + ν∇²_above*v[k-1]
-        temp_tend[k] += ν∇²_below*temp[k+1] - ν∇²_at_k*temp[k] + ν∇²_above*temp[k-1]
-    end
+        # full Laplacian in other layers
+        for k in 2:nlev-1
+            ν∇²_above = ν[k-1]*∇²_above[k]
+            ν∇²_below = ν[k]*∇²_below[k]
+            ν∇²_at_k = ν∇²_above + ν∇²_below
+            u_tend[k] += ν∇²_below*u[k+1] - ν∇²_at_k*u[k] + ν∇²_above*u[k-1]
+            v_tend[k] += ν∇²_below*v[k+1] - ν∇²_at_k*v[k] + ν∇²_above*v[k-1]
+            temp_tend[k] += ν∇²_below*temp[k+1] - ν∇²_at_k*temp[k] + ν∇²_above*temp[k-1]
+        end
 
-    # bottom layer with no flux boundary conditions at k=nlev+1/2
-    ν∇²_above = ν[end]*∇²_above[end]
-    u_tend[end] += ν∇²_above*(u[end-1] - u[end])
-    v_tend[end] += ν∇²_above*(v[end-1] - v[end])
-    temp_tend[end] += ν∇²_above*(temp[end-1] - temp[end])
+        # bottom layer with no flux boundary conditions at k=nlev+1/2
+        ν∇²_above = ν[end]*∇²_above[end]
+        u_tend[end] += ν∇²_above*(u[end-1] - u[end])
+        v_tend[end] += ν∇²_above*(v[end-1] - v[end])
+        temp_tend[end] += ν∇²_above*(temp[end-1] - temp[end])
     end
 
     return nothing
