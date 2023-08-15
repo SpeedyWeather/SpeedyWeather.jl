90d72339ceac82d51df6d88fd7282237943417e6
Milan Klöwer <milankloewer@gmx.de>
2023-08-15 11:27:28 -0400

    CenteredVerticalAdvection default

--- src/dynamics/models.jl
+++ src/dynamics/models.jl
@@ -139,7 +139,7 @@ Base.@kwdef struct PrimitiveDryModel{NF<:AbstractFloat, D<:AbstractDevice} <: Pr
     boundary_layer_drag::BoundaryLayerDrag{NF} = LinearDrag(spectral_grid)
     temperature_relaxation::TemperatureRelaxation{NF} = HeldSuarez(spectral_grid)
     static_energy_diffusion::VerticalDiffusion{NF} = StaticEnergyDiffusion(spectral_grid)
-    vertical_advection::VerticalAdvection{NF} = UpwindVerticalAdvection(spectral_grid)
+    vertical_advection::VerticalAdvection{NF} = CenteredVerticalAdvection(spectral_grid)
     # vertical_diffusion::VerticalDiffusion{NF} = VerticalLaplacian(spectral_grid)
 
     # NUMERICS
@@ -207,7 +207,7 @@ Base.@kwdef struct PrimitiveWetModel{NF<:AbstractFloat, D<:AbstractDevice} <: Pr
     temperature_relaxation::TemperatureRelaxation{NF} = HeldSuarez(spectral_grid)
     static_energy_diffusion::VerticalDiffusion{NF} = StaticEnergyDiffusion(spectral_grid)
     large_scale_condensation::AbstractCondensation{NF} = SpeedyCondensation(spectral_grid)
-    vertical_advection::VerticalAdvection{NF} = UpwindVerticalAdvection(spectral_grid)
+    vertical_advection::VerticalAdvection{NF} = CenteredVerticalAdvection(spectral_grid)
 
     # vertical_diffusion::VerticalDiffusion{NF} = VerticalLaplacian(spectral_grid)
 
