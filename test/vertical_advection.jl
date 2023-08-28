6db585d0161b5b6f632e14e1c19d39f1b4979cba
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-08-28 10:48:17 -0400

    add some tests?

---
+++ test/vertical_advection.jl
@@ -0,0 +1,15 @@
+using SpeedyWeather: WENOVerticalAdvection, CenteredVerticalAdvection, UpwindVerticalAdvection
+using SpeedyWeather: vertical_advection!
+
+@testset "Vertical advection runs" begin
+    spectral_grid = SpectralGrid()
+    model_types = (PrimitiveDry, PrimitiveWet)
+    advection_schems = (WENOVerticalAdvection, CenteredVerticalAdvection, UpwindVerticalAdvection)
+
+    for Model in model_types, VerticalAdvection in advection_schems
+        model = Model(; spectral_grid, vertical_advection = VerticalAdvection(spectral_grid))
+        simulation = initialize!(model)
+        run!(simulation,n_days=10)
+        @test simulation.model.feedback.nars_detected == false    
+    end
+end
