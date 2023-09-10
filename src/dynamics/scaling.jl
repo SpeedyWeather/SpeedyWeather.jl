96fd5f925dd040777d4d68589b33ecc5de8850a9
Milan <milankloewer@gmx.de>
2023-09-10 11:04:20 -0400

    scale_coslat funcs without geometry

--- src/dynamics/scaling.jl
+++ src/dynamics/scaling.jl
@@ -1,19 +1,19 @@
 # alias functions to scale the latitude of any gridded map A
-scale_coslat!(  A::AbstractGrid,G::Geometry) = _scale_lat!(A,G.coslat)
-scale_coslat²!( A::AbstractGrid,G::Geometry) = _scale_lat!(A,G.coslat²)
-scale_coslat⁻¹!(A::AbstractGrid,G::Geometry) = _scale_lat!(A,G.coslat⁻¹)
-scale_coslat⁻²!(A::AbstractGrid,G::Geometry) = _scale_lat!(A,G.coslat⁻²)
+scale_coslat!(  A::AbstractGrid) = _scale_coslat!(A,power=1)
+scale_coslat²!( A::AbstractGrid) = _scale_coslat!(A,power=2)
+scale_coslat⁻¹!(A::AbstractGrid) = _scale_coslat!(A,power=-1)
+scale_coslat⁻²!(A::AbstractGrid) = _scale_coslat!(A,power=-2)
 
-# matrix versions used for output
-scale_coslat!(  A::AbstractMatrix,G::Geometry) = A.*G.coslat'
-scale_coslat²!( A::AbstractMatrix,G::Geometry) = A.*G.coslat²'
-scale_coslat⁻¹!(A::AbstractMatrix,G::Geometry) = A.*G.coslat⁻¹'
-scale_coslat⁻²!(A::AbstractMatrix,G::Geometry) = A.*G.coslat⁻²'
+function _scale_coslat!(A::Grid;power=1) where {Grid<:AbstractGrid}
+    coslat = sin.(RingGrids.get_colat(Grid,A.nlat_half))    # sin(colat) = cos(lat)
+    coslat .^= power
+    return _scale_lat!(A,coslat)
+end
 
 """
 $(TYPEDSIGNATURES)
 Generic latitude scaling applied to `A` in-place with latitude-like vector `v`."""
-function _scale_lat!(A::AbstractGrid{NF},v::AbstractVector) where {NF<:AbstractFloat}
+function _scale_lat!(A::AbstractGrid{NF},v::AbstractVector) where NF
     @boundscheck get_nlat(A) == length(v) || throw(BoundsError)
     
     rings = eachring(A)
@@ -24,6 +24,8 @@ function _scale_lat!(A::AbstractGrid{NF},v::AbstractVector) where {NF<:AbstractF
             A[ij] *= vj
         end
     end
+
+    return A
 end 
 
 """
