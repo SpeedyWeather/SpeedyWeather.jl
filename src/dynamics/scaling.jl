6c8816514f45fad8e711b31616bcd087f78b5b96
Milan <milankloewer@gmx.de>
2023-09-10 11:35:16 -0400

    scale_coslat removed from SpeedyWeather

--- src/dynamics/scaling.jl
+++ src/dynamics/scaling.jl
@@ -1,33 +1,3 @@
-# alias functions to scale the latitude of any gridded map A
-scale_coslat!(  A::AbstractGrid) = _scale_coslat!(A,power=1)
-scale_coslat²!( A::AbstractGrid) = _scale_coslat!(A,power=2)
-scale_coslat⁻¹!(A::AbstractGrid) = _scale_coslat!(A,power=-1)
-scale_coslat⁻²!(A::AbstractGrid) = _scale_coslat!(A,power=-2)
-
-function _scale_coslat!(A::Grid;power=1) where {Grid<:AbstractGrid}
-    coslat = sin.(RingGrids.get_colat(Grid,A.nlat_half))    # sin(colat) = cos(lat)
-    coslat .^= power
-    return _scale_lat!(A,coslat)
-end
-
-"""
-$(TYPEDSIGNATURES)
-Generic latitude scaling applied to `A` in-place with latitude-like vector `v`."""
-function _scale_lat!(A::AbstractGrid{NF},v::AbstractVector) where NF
-    @boundscheck get_nlat(A) == length(v) || throw(BoundsError)
-    
-    rings = eachring(A)
-    
-    @inbounds for (j,ring) in enumerate(rings)
-        vj = convert(NF,v[j])
-        for ij in ring
-            A[ij] *= vj
-        end
-    end
-
-    return A
-end 
-
 """
 $(TYPEDSIGNATURES)
 Scale the variable `var` inside `progn` with scalar `scale`.
