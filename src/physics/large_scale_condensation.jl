09939294ba8cd32f6b237ad8de0d28ea098d4724
Milan <milankloewer@gmx.de>
2023-10-25 18:51:16 -0400

    Large-scale condensation simplified

--- src/physics/large_scale_condensation.jl
+++ src/physics/large_scale_condensation.jl
@@ -6,22 +6,17 @@ Base.@kwdef struct SpeedyCondensation{NF<:AbstractFloat} <: AbstractCondensation
     "number of vertical levels"
     nlev::Int
 
-    "Relative humidity threshold for boundary layer"
-    threshold_boundary_layer::Float64 = 0.95
+    "Relative humidity threshold"
+    relative_threshold::Float64 = 0.95
 
-    "Vertical range of relative humidity threshold"
-    threshold_range::Float64 = 0.1
-
-    "Maximum relative humidity threshold [1]"
-    threshold_max::Float64 = 0.9
+    "Relative humidity baseline for relaxation"
+    relative_baseline::Float64 = 0.9
 
     "Relaxation time for humidity [hrs]"
     time_scale::Float64 = 4.0
 
     # precomputed arrays
     n_stratosphere_levels::Base.RefValue{Int} = Ref(0)
-    humid_tend_max::Vector{NF} = zeros(NF,nlev)
-    relative_threshold::Vector{NF} = zeros(NF,nlev)
 end
 
 SpeedyCondensation(SG::SpectralGrid;kwargs...) = SpeedyCondensation{SG.NF}(nlev=SG.nlev;kwargs...)
@@ -31,25 +26,10 @@ $(TYPEDSIGNATURES)
 Initialize the SpeedyCondensation scheme."""
 function initialize!(scheme::SpeedyCondensation,model::PrimitiveEquation)
 
-    (;nlev,threshold_boundary_layer,threshold_range,threshold_max) = scheme
-    (;humid_tend_max, relative_threshold, time_scale) = scheme
     (;σ_levels_full) = model.geometry
-    (;σ_tropopause,σ_boundary_layer) = model.atmosphere
+    (;σ_tropopause) = model.atmosphere
 
-    scheme.n_stratosphere_levels[] = findfirst(σ->σ>=σ_tropopause,σ_levels_full)
-
-    for k in 1:nlev
-        # the relative humidity threshold above which condensation occurs per layer
-        σₖ² = σ_levels_full[k]^2
-        relative_threshold[k] = threshold_max + threshold_range * (σₖ² - 1)
-        if σ_levels_full[k] >= σ_boundary_layer
-            relative_threshold[k] = max(relative_threshold[k], threshold_boundary_layer)
-        end
-
-        # Impose a maximum heating rate to avoid grid-point storm instability
-        # This formula does not appear in the original Speedy documentation
-        humid_tend_max[k] = 10σₖ² / 3600time_scale          # [hrs] → [s]
-    end
+    scheme.n_stratosphere_levels[] = findlast(σ->σ<=σ_tropopause,σ_levels_full)
 end
 
 """
@@ -86,7 +66,7 @@ function large_scale_condensation!(
     time_stepping::TimeStepper,
     ) where NF
 
-    (;relative_threshold,humid_tend_max) = scheme
+    (;relative_threshold,relative_baseline) = scheme
     time_scale = convert(NF,3600*scheme.time_scale)     # [hrs] -> [s]
     n_stratosphere_levels = scheme.n_stratosphere_levels[]
 
@@ -107,11 +87,13 @@ function large_scale_condensation!(
     @inbounds for k in n_stratosphere_levels+1:nlev   # top to bottom, skip stratospheric levels
 
         # Specific humidity threshold for condensation
-        humid_threshold = relative_threshold[k] * sat_humid[k]               
+        humid_threshold = relative_threshold * sat_humid[k]               
+        humid_baseline = relative_baseline * sat_humid[k]               
 
         if humid[k] > humid_threshold
+
             # accumulate in tendencies (nothing is added if humidity not above threshold)
-            humid_tend_k = -(humid[k] - humid_threshold) / time_scale                   # Formula 22
+            humid_tend_k = -(humid[k] - humid_baseline) / time_scale                   # Formula 22
             # temp_tend[k] += -latent_heat * min(humid_tend_k, humid_tend_max[k]*pres[k]) # Formula 23
             temp_tend[k] += -latent_heat * humid_tend_k             # Formula 23, without limiter
 
@@ -124,7 +106,7 @@ function large_scale_condensation!(
             ΔpₖΔt_gρ = σ_levels_thick[k]*pₛΔt_gρ                    # Formula 4 *Δt for [m] of rain during Δt
             column.precip_large_scale += -ΔpₖΔt_gρ * humid_tend_k   # Formula 25, unit [m]
 
-            # only write into humid_tend now to allow humid_tend != 0 before this scheme is called
+            # only accumulate into humid_tend now to allow humid_tend != 0 before this scheme is called
             humid_tend[k] += humid_tend_k
         end
     end
