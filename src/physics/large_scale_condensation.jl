51b1a25f3882e20d5eb26bb25dba0e664f66aedf
Milan <milankloewer@gmx.de>
2023-11-13 17:31:47 -0500

    large scale condensation had 1/cp missing

--- src/physics/large_scale_condensation.jl
+++ src/physics/large_scale_condensation.jl
@@ -7,16 +7,17 @@ Base.@kwdef struct SpeedyCondensation{NF<:AbstractFloat} <: AbstractCondensation
     nlev::Int
 
     "Relative humidity threshold"
-    relative_threshold::Float64 = 0.95
+    relative_threshold::NF = 0.95
 
     "Relative humidity baseline for relaxation"
-    relative_baseline::Float64 = 0.9
+    relative_baseline::NF = 0.9
 
     "Relaxation time for humidity [hrs]"
     time_scale::Second = Hour(4)
 
     # precomputed arrays
     n_stratosphere_levels::Base.RefValue{Int} = Ref(0)
+    latent_heat_cₚ::Base.RefValue{NF} = Ref(zero(NF))
 end
 
 SpeedyCondensation(SG::SpectralGrid;kwargs...) = SpeedyCondensation{SG.NF}(nlev=SG.nlev;kwargs...)
@@ -31,6 +32,11 @@ function initialize!(scheme::SpeedyCondensation,model::PrimitiveEquation)
 
     n = findlast(σ->σ<=σ_tropopause,σ_levels_full)
     scheme.n_stratosphere_levels[] = isnothing(n) ? 0 : n
+
+    # to convert humidity tendency to temperature tendency
+    # Fortran SPEEDY documentation eq. (23)
+    scheme.latent_heat_cₚ[] = model.atmosphere.latent_heat_condensation/    # [J/kg]
+                                model.atmosphere.cₚ     # [J/K/kg] specific heat capacity, const pres
 end
 
 """
@@ -49,7 +55,7 @@ function large_scale_condensation!(
     model::PrimitiveWet,
 )
     large_scale_condensation!(column,model.large_scale_condensation,
-        model.geometry,model.constants,model.atmosphere,model.time_stepping)
+        model.geometry,model.constants,model.time_stepping)
 end
 
 """
@@ -63,7 +69,6 @@ function large_scale_condensation!(
     scheme::SpeedyCondensation,
     geometry::Geometry,
     constants::DynamicsConstants,
-    atmosphere::AbstractAtmosphere,
     time_stepping::TimeStepper,
     ) where NF
 
@@ -71,19 +76,18 @@ function large_scale_condensation!(
     time_scale⁻¹ = inv(convert(NF,scheme.time_scale.value))
     n_stratosphere_levels = scheme.n_stratosphere_levels[]
 
-    (;humid, pres) = column               # prognostic variables: specific humidity, surface pressure
-    (;temp_tend, humid_tend) = column     # tendencies to write into
-    (;sat_humid) = column                 # intermediate variable, calculate in thermodynamics!
+    (;humid, pres) = column                 # prognostic vars: specific humidity, surface pressure
+    (;temp_tend, humid_tend) = column       # tendencies to write into
+    (;sat_humid) = column                   # intermediate variable, calculated in thermodynamics!
     (;nlev) = column
-    pₛ = pres[end]                         # surface pressure
+    pₛ = pres[end]                          # surface pressure
 
     (;gravity, water_density) = constants
     (;Δt_sec) = time_stepping
     pₛΔt_gρ = pₛ*Δt_sec/gravity/water_density   # precompute constant
 
     (;σ_levels_thick) = geometry
-    latent_heat = convert(NF, atmosphere.latent_heat_condensation)
-    
+
     # 1. Tendencies of humidity and temperature due to large-scale condensation
     @inbounds for k in n_stratosphere_levels+1:nlev   # top to bottom, skip stratospheric levels
 
@@ -96,11 +100,11 @@ function large_scale_condensation!(
             # accumulate in tendencies (nothing is added if humidity not above threshold)
             humid_tend_k = -(humid[k] - humid_baseline) * time_scale⁻¹                  # Formula 22
             # temp_tend[k] += -latent_heat * min(humid_tend_k, humid_tend_max[k]*pres[k]) # Formula 23
-            temp_tend[k] += -latent_heat * humid_tend_k             # Formula 23, without limiter
+            temp_tend[k] += -scheme.latent_heat_cₚ[] * humid_tend_k             # Formula 23, without limiter
 
             # If there is large-scale condensation at a level higher (i.e. smaller k) than
             # the cloud-top previously diagnosed due to convection, then increase the cloud-top
-            # column.cloud_top = min(column.cloud_top, k)             # Page 7 (last sentence)
+            column.cloud_top = min(column.cloud_top, k)             # Page 7 (last sentence)
     
             # 2. Precipitation due to large-scale condensation [kg/m²/s] /ρ for [m/s]
             # += for vertical integral
