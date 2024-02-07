2064e9751a86fe234ab9fc3d7d78a4334d094673
Milan <milankloewer@gmx.de>
2024-02-07 15:18:02 -0500

    BulkRichardsonDrag implemented

--- src/physics/boundary_layer.jl
+++ src/physics/boundary_layer.jl
@@ -1,3 +1,5 @@
+abstract type BoundaryLayerDrag{NF} <: AbstractParameterization{NF} end
+
 """Concrete type that disables the boundary layer drag scheme."""
 struct NoBoundaryLayerDrag{NF} <: BoundaryLayerDrag{NF} end
 
@@ -9,15 +11,16 @@ function initialize!(   scheme::NoBoundaryLayerDrag,
     return nothing
 end 
 
-# function barrier
+# function barrier for all boundary layer drags
 function boundary_layer_drag!(  column::ColumnVariables,
                                 model::PrimitiveEquation)
-    boundary_layer_drag!(column,model.boundary_layer_drag)
+    boundary_layer_drag!(column,model.boundary_layer_drag,model)
 end
 
 """NoBoundaryLayer scheme just passes."""
 function boundary_layer_drag!(  column::ColumnVariables,
-                                scheme::NoBoundaryLayerDrag)
+                                scheme::NoBoundaryLayerDrag,
+                                model::PrimitiveEquation)
     return nothing
 end
 
@@ -57,6 +60,13 @@ function initialize!(   scheme::LinearDrag,
     end
 end 
 
+# function barrier
+function boundary_layer_drag!(  column::ColumnVariables,
+                                scheme::LinearDrag,
+                                model::PrimitiveEquation)
+    boundary_layer_drag!(column,scheme)
+end
+
 """
 $(TYPEDSIGNATURES)
 Compute tendency for boundary layer drag of a `column` and add to its tendencies fields"""
@@ -74,3 +84,80 @@ function boundary_layer_drag!(  column::ColumnVariables,
         end
     end
 end
+
+Base.@kwdef struct ConstantDrag{NF} <: BoundaryLayerDrag{NF}
+    drag::NF = 1e-3
+end
+
+ConstantDrag(SG::SpectralGrid;kwargs...) = ConstantDrag{SG.NF}(;kwargs...)
+
+initialize!(::ConstantDrag,::PrimitiveEquation) = nothing
+
+function boundary_layer_drag!(  column::ColumnVariables,
+                                scheme::ConstantDrag,
+                                model::PrimitiveEquation)
+    column.boundary_layer_drag = scheme.drag
+end
+
+"""Boundary layer drag coefficient from the bulk Richardson number,
+following Frierson, 2006, Journal of the Atmospheric Sciences.
+$(TYPEDFIELDS)"""
+Base.@kwdef struct BulkRichardsonDrag{NF} <: BoundaryLayerDrag{NF}
+    "von Kármán constant [1]"
+    κ::NF = 0.4
+
+    "roughness length [m]"
+    z₀::NF = 3.21e-5
+
+    "Critical Richardson number for stable mixing cutoff [1]"
+    Ri_c::NF = 1
+
+    "Maximum drag coefficient, κ²/log(zₐ/z₀) for zₐ from reference temperature"
+    drag_max::Base.RefValue{NF} = Ref(zero(NF))
+end
+
+BulkRichardsonDrag(SG::SpectralGrid,kwargs...) = BulkRichardsonDrag{SG.NF}(;kwargs...)
+
+function initialize!(scheme::BulkRichardsonDrag, model::PrimitiveEquation)
+
+    # Typical height Z of lowermost layer from geopotential of reference surface temperature
+    # minus surface geopotential (orography * gravity)
+    (;temp_ref) = model.atmosphere
+    (;Δp_geopot_full, gravity) = model.constants
+    Z = temp_ref*Δp_geopot_full[end] / gravity
+    
+    # maximum drag Cmax from that height, stable conditions would decrease Cmax towards 0
+    # Frierson 2006, eq (12)
+    (;κ, z₀) = scheme
+    scheme.drag_max[] = (κ/log(Z/z₀))^2
+end
+
+function boundary_layer_drag!(  column::ColumnVariables,
+                                scheme::BulkRichardsonDrag,
+                                model::PrimitiveEquation)
+    boundary_layer_drag!(column,scheme)
+end
+
+function boundary_layer_drag!(
+    column::ColumnVariables,
+    scheme::BulkRichardsonDrag,
+)
+    
+    (;Ri_c) = scheme
+    drag_max = scheme.drag_max[]
+
+    # bulk Richardson number at lowermost layer from Frierson, 2006, eq. (15)
+    Ri_a = column.bulk_richardson[column.nlev]
+
+    # clamp to get the cases, eq (12-14)
+    # if Ri_a > Ri_c then C = 0
+    # if Ri_c > Ri_a > 0 then = κ^2/log(zₐ/z₀)^2 * (1-Ri_a/Ri_c)^2
+    # if Ri_c < 0 then κ^2/log(zₐ/z₀)^2
+    # but Z ≈ zₐ given that the lowermost layer height is proportional to temperature
+    # which doesn't change much with instantaneous temperature variations but with
+    # vertical resolution, hence κ^2/log(Z/z₀)^2 is precalculated in initialize!
+    Ri_a = clamp(Ri_a, 0, Ri_c)
+    column.boundary_layer_drag = drag_max*(1-Ri_a/Ri_c)^2
+end
+
+
