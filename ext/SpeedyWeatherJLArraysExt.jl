7f4bf3812b1409dc36f0e254283dd71cbecc5f96
Milan Klöwer <milankloewer@gmx.de>
2024-04-25 11:56:40 -0400

    nonparametric_type in LTA for JLArrays moved to extension

--- ext/SpeedyWeatherJLArraysExt.jl
+++ ext/SpeedyWeatherJLArraysExt.jl
@@ -2,7 +2,9 @@ module SpeedyWeatherJLArraysExt
 
 using SpeedyWeather, JLArrays
 
-# for RingGrids, every Array needs this method to strip away the parameters
+# for RingGrids and LowerTriangularMatrices:
+# every Array needs this method to strip away the parameters
 SpeedyWeather.RingGrids.nonparametric_type(::Type{JLArray}) = JLArray
+SpeedyWeather.LowerTriangularMatrices.nonparametric_type(::Type{JLArray}) = JLArray
 
 end # module
