eb8725bbe063c30bdb48803712618a9830cbdf07
Max <maximilian.gelbrecht@posteo.de>
2024-05-22 10:11:11 +0200

    fast cartesian indexing + broadcasting

--- src/LowerTriangularMatrices/lower_triangular_matrix.jl
+++ src/LowerTriangularMatrices/lower_triangular_matrix.jl
@@ -155,6 +155,7 @@ end
 
 # important to do Tuple(I) here for the j > i case as one of the getindex methods above is called
 Base.@propagate_inbounds Base.getindex(L::LowerTriangularArray{T,N}, I::CartesianIndex{M}) where {T,N,M} = getindex(L, Tuple(I)...)
+Base.@propagate_inbounds Base.getindex(L::LowerTriangularArray{T,N}, I::CartesianIndex{N}) where {T,N} = getindex(L.data, I)
 
 Base.@propagate_inbounds Base.getindex(L::LowerTriangularArray{T,N}, i::Integer) where {T,N} = getindex(L.data, i)
 
@@ -179,6 +180,7 @@ end
 
 
 @inline Base.setindex!(L::LowerTriangularArray, x, I::CartesianIndex) = setindex!(L, x, Tuple(I)...)
+@inline Base.setindex!(L::LowerTriangularArray{T,N}, x, I::CartesianIndex{N}) where {T,N} = setindex!(L.data, x, I)
 @inline Base.setindex!(L::LowerTriangularArray{T,N}, x, i::Integer) where {T,N} = setindex!(L.data, x, i)
 
 # this is to avoid ambigouities
@@ -490,7 +492,7 @@ struct LowerTriangularGPUStyle{N, ArrayType} <: GPUArrays.AbstractGPUArrayStyle{
 function BroadcastStyle(::Type{LowerTriangularArray{T, N, ArrayType}}) where {T, N, ArrayType}
     # remove number format parameter for broadcasting with type promotion
     ArrayType_ = nonparametric_type(ArrayType)
-    return LowerTriangularStyle{N, ArrayType_, }()
+    return LowerTriangularStyle{N, ArrayType_}()
 end
 
 function BroadcastStyle(
@@ -534,7 +536,7 @@ function Base.similar(
     # but given the TODO above return a `LowerTriangularArray` in both cases
     # return similar(convert(Broadcasted{DefaultArrayStyle{ndims(bc)}}, bc), T)
     L = find_L(bc)
-    return LowerTriangularArray{T, N, ArrayType{T,N}}(undef, L.m, L.n)
+    return LowerTriangularArray{T, N, ArrayType{T,N}}(undef, matrix_size(L))
 end
 
 # same function as above, but needs to be defined for both CPU and GPU style
@@ -552,8 +554,67 @@ function Base.similar(
     # but given the TODO above return a `LowerTriangularArray` in both cases
     # return similar(convert(Broadcasted{DefaultArrayStyle{ndims(bc)}}, bc), T)
     L = find_L(bc)
-    return LowerTriangularArray{T, N, ArrayType{T,N}}(undef, L.m, L.n)
+    return LowerTriangularArray{T, N, ArrayType{T,N}}(undef, matrix_size(L))
 end
+
+"""
+function Base.copyto!(
+    dest::LowerTriangularArray{T, N, ArrayTypeP},
+    bc::Broadcasted{LowerTriangularStyle{N, ArrayType}},
+) where {T, N, ArrayTypeP <: Array, ArrayType <: Array}
+    axs = axes(dest)
+    axes(bc) == axs || Broadcast.throwdm(axes(bc), axs)
+
+    lmax, mmax = size(dest)
+    lm = 0
+
+    for m in 1:mmax
+        for l in m:lmax
+            lm += 1
+            for I in Iterators.product(axs[3:end]...)
+                dest.data[lm, I...] = Broadcast._broadcast_getindex(bc, CartesianIndex(l, m, I...))
+            end 
+        end
+    end
+    return dest
+end
+
+# GPU methods (should be moved to an extension, in case this becomes a standalone package)
+import GPUArrays._copyto!
+
+# copied and adjusted from GPUArrays.jl
+@inline function GPUArrays._copyto!(
+    dest::LowerTriangularArray{T, N, ArrayType},
+    bc::Broadcasted
+) where {T, N, ArrayType}
+    
+    axes(dest) == axes(bc) || Broadcast.throwdm(axes(dest), axes(bc))
+    isempty(dest) && return dest
+    bc = Broadcast.preprocess(dest, bc)
+
+    broadcast_kernel = function (ctx, dest, bc, nelem)
+            i = 0
+            while i < nelem
+                i += 1
+                I = GPUArrays.@cartesianidx(dest.data, i) 
+                # TODO: the k2ij is costly, if we intend to accelarate this, this needs to be precomputed
+                @inbounds dest.data[I] = bc[k2ij(I, dest.m)]
+            end                                                                     
+            return
+        end
+
+    elements = length(dest.data)
+    elements_per_thread = typemax(Int)
+    heuristic = GPUArrays.launch_heuristic(GPUArrays.backend(dest), broadcast_kernel, dest, bc, 1;
+                                 elements, elements_per_thread)
+    config = GPUArrays.launch_configuration(GPUArrays.backend(dest), heuristic;
+                                  elements, elements_per_thread)
+    GPUArrays.gpu_call(broadcast_kernel, dest, bc, config.elements_per_thread;
+             threads=config.threads, blocks=config.blocks)
+
+    return dest
+end
+"""
 
 function GPUArrays.backend(
     ::Type{LowerTriangularArray{T, N, ArrayType}}
