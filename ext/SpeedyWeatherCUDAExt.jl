2040596233fd7385f5d11ddae03492afa8f8242c
Milan <milankloewer@gmx.de>
2024-10-10 15:06:28 +0100

    move Device() into CUDA extension

--- ext/SpeedyWeatherCUDAExt.jl
+++ ext/SpeedyWeatherCUDAExt.jl
@@ -10,13 +10,13 @@ LowerTriangularMatrices.nonparametric_type(::Type{<:CuArray}) = CuArray
 
 SpeedyWeather.default_array_type(::Type{GPU}) = CuArray
 
-SpeedyWeather.DeviceSetup() = DeviceSetup(Device(), Device_KernelAbstractions(Device()), workgroup_size(Device()))
 
 # DEVICE SETUP FOR CUDA
 
 """$(TYPEDSIGNATURES)
 Return default used device for internal purposes, either `CPU` or `GPU` if a GPU is available."""
-SpeedyWeather.Device() = CUDA.functional() ? GPU() : CPU()
+Device() = CUDA.functional() ? GPU() : CPU()
+SpeedyWeather.DeviceSetup() = DeviceSetup(Device(), Device_KernelAbstractions(Device()), workgroup_size(Device()))
 
 """$(TYPEDSIGNATURES)
 Return default used device for KernelAbstractions, either `CPU` or `CUDADevice` if a GPU is available."""
