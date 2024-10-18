beacd24fc949b2d42ad7e2f1bbcf3b19ffe27ff6
Milan <milankloewer@gmx.de>
2024-10-18 13:22:28 +0100

    add type stability test

--- test/geodesics.jl
+++ test/geodesics.jl
@@ -1,13 +1,24 @@
-@testset "Haversine Earth" begin
-    EARTH_CIRCUMFERENCE = 40075016.69  # Earth circumference in meters
+@testset "Spherical distance on Earth" begin
+    earth_circumference = 2π*RingGrids.DEFAULT_RADIUS
     for (point1, point2, expected) in [
-        ((0, 0), (1, 0), EARTH_CIRCUMFERENCE / 360),
-        ((0, 0), (359, 0), EARTH_CIRCUMFERENCE / 360),
-        ((0, 0), (180, 0), EARTH_CIRCUMFERENCE / 2),
-        ((0, 0), (90, 0), EARTH_CIRCUMFERENCE / 4),
+
+        # longitude 
+        ((0, 0), (1, 0), earth_circumference / 360),
+        ((0, 0), (359, 0), earth_circumference / 360),
+        ((0, 0), (180, 0), earth_circumference / 2),
+        ((0, 0), (90, 0), earth_circumference / 4),
+
+        # latitude
+        ((0, 0), (0, 1), earth_circumference / 360),
+        ((0, 0), (0, 10), 10*earth_circumference / 360),
+        ((0, 0), (0, 90), earth_circumference / 4),
+        ((0, 0), (0, -90), earth_circumference / 4),
     ]
-        @test RingGrids.Haversine(point1, point2) ≈ expected rtol=0.05
+        @test spherical_distance(point1, point2) ≈ expected
     end
+end
+
+@testset "Spherical distance invariants" begin
 
     # Ensure that function preserves the invariant that shifting in the longitude
     # direction does not change the distance.
@@ -18,22 +29,22 @@
         lon_shift = 360 * rand() 
         
         # Invariance in longitude shift.
-        @test RingGrids.Haversine(p1, p2) ≈ RingGrids.Haversine(shift(p1, lon_shift), shift(p2, lon_shift))
+        @test spherical_distance(p1, p2) ≈ spherical_distance(shift(p1, lon_shift), shift(p2, lon_shift))
         # Commutative.
-        @test RingGrids.Haversine(p1, p2) ≈ RingGrids.Haversine(p2, p1)
+        @test spherical_distance(p1, p2) ≈ spherical_distance(p2, p1)
         # Symmetry.
-        @test RingGrids.Haversine(p1, p2) == RingGrids.Haversine((-p1[1], -p1[2]), (-p2[1], -p2[2]))
+        @test spherical_distance(p1, p2) == spherical_distance((-p1[1], -p1[2]), (-p2[1], -p2[2]))
         # Identity.
-        @test RingGrids.Haversine(p1, p1) == 0
+        @test spherical_distance(p1, p1) == 0
 
         # Ensure that different ways to call the function are identical.
-        @test RingGrids.Haversine(p1, p2) == RingGrids.Haversine(p1[1], p1[2], p2[1], p2[2])
-        @test RingGrids.Haversine(p1, p2) == RingGrids.spherical_distance(p1, p2)
-        @test RingGrids.Haversine(p1, p2) == RingGrids.spherical_distance(RingGrids.Haversine, p1, p2)
+        @test spherical_distance(p1, p2) == spherical_distance(p1[1], p1[2], p2[1], p2[2])
+        @test spherical_distance(p1, p2) == spherical_distance(p1, p2)
+        @test spherical_distance(p1, p2) == spherical_distance(RingGrids.Haversine, p1, p2)
     end
 end
 
-@testset "Haversine Custom Radius" begin
+@testset "Spherical distance with custom radius" begin
     custom_radius = 100
     circumference = 2 * π * custom_radius
     for (point1, point2, expected) in [
@@ -41,8 +52,31 @@ end
         ((0, 0), (359, 0), circumference / 360),
         ((0, 0), (180, 0), circumference / 2),
         ((0, 0), (90, 0), circumference / 4),
+
+        ((0, 0), (0, 1), circumference / 360),
+        ((0, 0), (0, 10), 10*circumference / 360),
+        ((0, 0), (0, 90), circumference / 4),
+        ((0, 0), (0, -90), circumference / 4),
     ]
         # There's a small numerical error, hence only approximate equality.
-        @test RingGrids.Haversine(point1, point2, radius=custom_radius) ≈ expected
+        @test spherical_distance(point1, point2, radius=custom_radius) ≈ expected
+    end
+end
+
+@testset "Spherical distance type stability" begin
+    for T in (Float16, Float32, Float64)
+        p1, p2 = (rand(T), rand(T)), (rand(T), rand(T))
+        @test typeof(spherical_distance(p1, p2)) == T
+
+        # but if radius provided promote to that type
+        for Tradius in (Float16, Float32, Float64)
+            @test typeof(spherical_distance(p1, p2, radius=Tradius(1))) == promote_type(T, Tradius)
+        end
+    end
+
+    # but integers always go to Float64 due to the trigonometric functions
+    for T in (Int16, Int32, Int64)
+        p1, p2 = (rand(T), rand(T)), (rand(T), rand(T))
+        @test typeof(spherical_distance(p1, p2)) == Float64
     end
 end
