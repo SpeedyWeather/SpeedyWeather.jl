227c98fdeff96619d13c714831d841c043a8f479
Max <maximilian.gelbrecht@posteo.de>
2024-10-18 12:01:46 +0200

    start FiniteDifferencesExtension

---
+++ ext/SpeedyWeatherFiniteDifferencesExt.jl
@@ -0,0 +1,20 @@
+module SpeedyWeatherFiniteDifferencesExt 
+
+using SpeedyWeather 
+import FiniteDifferences
+import FiniteDifferences: to_vec 
+
+# FiniteDifferences needs to be able to convert data structures to Vectors and back 
+# This doesn't work out of the box with our data types, so we'll define those 
+# conversions here.
+function FiniteDifferences.to_vec(x::Grid) where Grid <: AbstractGridArray
+    x_vec, from_vec = FiniteDifferences.to_vec(Array(x))
+
+    function GridArray_from_vec(x_vec)
+        return Grid(reshape(from_vec(x), size(x)), x.nlat_half)
+    end 
+
+    return x_vec, GridArray_from_vec
+end 
+
+end 
--- test/test_transforms_ad_rules.jl
+++ test/test_transforms_ad_rules.jl
@@ -1,27 +1,28 @@
-using EnzymeTestUtils, Enzyme, FiniteDifferences
+using SpeedyWeather
+using EnzymeTestUtils, Enzyme
+using FiniteDifferences
+import FiniteDifferences: j′vp, central_fdm
 
-grids = [FullGaussianGrid, OctahedralClenshawGrid] # one full and one reduced grid 
+grid_types = [FullGaussianGrid, OctahedralClenshawGrid] # one full and one reduced grid 
 
 @testset "SpeedyTransforms: AD Rules" begin
-
-
     @testset "_fourier! Enzyme rules" begin
         
         @testset "reverse rule" begin
-            for grid in grids,
+            for grid_type in grid_types,
                 Tf_n in (Const,),
                 Tf_s in (Const,),
                 Tf_grids in (Duplicated,),
                 Tf_S in (Const,),
                 fun in (SpeedyWeather.SpeedyTransforms._fourier!, )
 
-                spectral_grid = SpectralGrid(Grid=grid)
+                spectral_grid = SpectralGrid(Grid=grid_type)
                 S = SpectralTransform(spectral_grid)
                 grid = rand(spectral_grid.Grid{spectral_grid.NF}, spectral_grid.nlat_half, spectral_grid.nlayers)
                 f_north = S.scratch_memory_north
                 f_south = S.scratch_memory_south
 
-                test_reverse(fun, Const, (f_north, Tf_n), (f_south, Tf_s), (grid, Tf_grids), (S, Const))
+                test_reverse(fun, Const, (f_north, Tf_n), (f_south, Tf_s), (grid, Tf_grids), (S, Tf_S))
             end
         end
     end 
@@ -29,9 +30,9 @@ grids = [FullGaussianGrid, OctahedralClenshawGrid] # one full and one reduced gr
     @testset "Complete Transform Enzyme" begin
         # make a high level finite difference test of the whole transform
         # can't use Enzyme or ChainRule Test tools for tests for that
-        for grid in grids 
+        for grid_type in grids 
 
-            spectral_grid = SpectralGrid(Grid=grid)
+            spectral_grid = SpectralGrid(Grid=grid_type)
             S = SpectralTransform(spectral_grid)
             grid = rand(spectral_grid.Grid{spectral_grid.NF}, spectral_grid.nlat_half, spectral_grid.nlayers)
             dgrid = zero(grid)
@@ -39,7 +40,10 @@ grids = [FullGaussianGrid, OctahedralClenshawGrid] # one full and one reduced gr
 
             autodiff(Reverse, transform!, Const, Const(specs), Duplicated(grid, dgrid), Const(S))
 
-            # finite difference comparision 
+            # finite difference comparision
+
+            fd_jvpi = j′vp(central_fdm(5,1), x -> transform(x, S), specs, grid)
+ 
         end 
     end 
 
