0bc27ba03d8302a11643d30ec8dcca924512131b
Milan <milankloewer@gmx.de>
2024-11-08 14:26:24 +0000

    globe options

--- ext/SpeedyWeatherGeoMakieExt/SpeedyWeatherGeoMakieExt.jl
+++ ext/SpeedyWeatherGeoMakieExt/SpeedyWeatherGeoMakieExt.jl
@@ -10,30 +10,45 @@ include("faces.jl")
 SpeedyWeather.globe(SG::SpectralGrid) = globe(SG.Grid, SG.nlat_half)
 SpeedyWeather.globe(geometry::Geometry{NF, Grid}) where {NF, Grid} = globe(Grid, geometry.nlat_half)
 
-function SpeedyWeather.globe(Grid::Type{<:AbstractGridArray}, nlat_half::Integer)
-
+function SpeedyWeather.globe(
+    Grid::Type{<:AbstractGridArray},
+    nlat_half::Integer;
+    color = :black,
+    faces::Bool = true,
+    centers::Bool = true,
+    coastlines::Bool = true,
+    background::Bool = true,
+)
     transf = GeoMakie.Geodesy.ECEFfromLLA(GeoMakie.Geodesy.WGS84())
 
     fig = Figure(size=(800, 800));
     ax = LScene(fig[1,1], show_axis=false);
 
     # background image
-    bg = meshimage!(ax, -180..180, -90..90, rotr90(GeoMakie.earth()); npoints = 100, z_level = -10_000);
-    bg.transformation.transform_func[] = transf
+    if background
+        bg = meshimage!(ax, -180..180, -90..90, rotr90(GeoMakie.earth()); npoints = 100, z_level = -10_000);
+        bg.transformation.transform_func[] = transf
+    end
 
-    # cell centers
-    color = :black
-    latds, londs = RingGrids.get_latdlonds(Grid, nlat_half)
-    s = scatter!(ax, londs, latds, markersize=5; color)
-    s.transformation.transform_func[] = transf
+    # cell centers, i.e. the grid points
+    if centers
+        latds, londs = RingGrids.get_latdlonds(Grid, nlat_half)
+        c = scatter!(ax, londs, latds, markersize=5; color)
+        c.transformation.transform_func[] = transf
+    end
 
-    # cell faces, a vector of Point2, concanated all vertices for each grid point
-    faces = get_faces(Grid, nlat_half)
-    lp = lines!(ax, vec(faces); color)
+    # cell faces, a vector of Point2, concatenated all vertices for each grid point
+    if faces
+        faces = get_faces(Grid, nlat_half)
+        f = lines!(ax, vec(faces); color)
+        f.transformation.transform_func[] = transf
+    end
 
     # coastlines
-    lpc = lines!(GeoMakie.coastlines(50); color, linewidth=1)
-    lpc.transformation.transform_func[] = transf
+    if coastlines
+        cl = lines!(GeoMakie.coastlines(50); color, linewidth=1)
+        cl.transformation.transform_func[] = transf
+    end
 
     # Makie stuff
     cc = cameracontrols(ax.scene)
