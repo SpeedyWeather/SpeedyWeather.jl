c992c969aec2fcda2ca0aef2cfcd4ae2fd40c65d
Milan <milankloewer@gmx.de>
2024-11-11 15:11:11 +0000

    interactive bool

--- ext/SpeedyWeatherGeoMakieExt/SpeedyWeatherGeoMakieExt.jl
+++ ext/SpeedyWeatherGeoMakieExt/SpeedyWeatherGeoMakieExt.jl
@@ -16,28 +16,37 @@ cell centers and faces. Optionally, add coastlines and a background image of the
 function SpeedyWeather.globe(
     Grid::Type{<:AbstractGridArray},
     nlat_half::Integer;
+    interactive::Bool = true,
+    title::String = "$(RingGrids.get_nlat(typeof(grid), grid.nlat_half))-ring $(RingGrids.horizontal_grid_type(grid))",
     color = :black,
     faces::Bool = true,
     centers::Bool = true,
     coastlines::Bool = true,
     background::Bool = true,
 )
-    transf = GeoMakie.Geodesy.ECEFfromLLA(GeoMakie.Geodesy.WGS84())
 
     fig = Figure(size=(800, 800));
-    ax = LScene(fig[1,1], show_axis=false);
+
+    if interactive
+        transf = GeoMakie.Geodesy.ECEFfromLLA(GeoMakie.Geodesy.WGS84())
+        ax = LScene(fig[1,1], show_axis=false);
+    else
+        ax = GeoAxis(fig[1, 1],
+            title= title,
+            dest = "+proj=ortho +lon_0=30 +lat_0=45")
+    end
 
     # background image
     if background
         bg = meshimage!(ax, -180..180, -90..90, rotr90(GeoMakie.earth()); npoints = 100, z_level = -10_000);
-        bg.transformation.transform_func[] = transf
+        interactive && (bg.transformation.transform_func[] = transf)
     end
 
     # cell centers, i.e. the grid points
     if centers
         latds, londs = RingGrids.get_latdlonds(Grid, nlat_half)
         c = scatter!(ax, londs, latds, markersize=5; color)
-        c.transformation.transform_func[] = transf
+        interactive && (c.transformation.transform_func[] = transf)
     end
 
     # cell faces, a vector of Point2, concatenated all vertices for each grid point
@@ -45,20 +54,24 @@ function SpeedyWeather.globe(
         # add nan after every face to avoid lines linking grid cells
         faces = get_faces(Grid, nlat_half, add_nan=true)
         f = lines!(ax, vec(faces); color)
-        f.transformation.transform_func[] = transf
+        interactive && (f.transformation.transform_func[] = transf)
     end
 
     # coastlines
     if coastlines
         cl = lines!(GeoMakie.coastlines(50); color, linewidth=1)
-        cl.transformation.transform_func[] = transf
+        interactive && (cl.transformation.transform_func[] = transf)
     end
 
     # Makie stuff
-    cc = cameracontrols(ax.scene)
-    cc.settings.mouse_translationspeed[] = 0.0
-    cc.settings.zoom_shift_lookat[] = false
-    Makie.update_cam!(ax.scene, cc)
+    if interactive
+        cc = cameracontrols(ax.scene)
+        cc.settings.mouse_translationspeed[] = 0.0
+        cc.settings.zoom_shift_lookat[] = false
+        Makie.update_cam!(ax.scene, cc)
+    else
+        hidedecorations!(ax)
+    end
 
     return fig
 end
@@ -70,29 +83,42 @@ Create a 3D interactive globe plot of the data in `grid` displayed as polygons b
 the cell faces. Optionally, add coastlines (default true)."""
 function SpeedyWeather.globe(
     grid::AbstractGrid;
+    interactive::Bool = true,
+    title::String = "$(RingGrids.get_nlat(typeof(grid), grid.nlat_half))-ring $(RingGrids.horizontal_grid_type(grid))",
     colormap = :viridis,
     coastlines::Bool = true,
 )
-    transf = GeoMakie.Geodesy.ECEFfromLLA(GeoMakie.Geodesy.WGS84())
 
     fig = Figure(size=(800, 800));
-    ax = LScene(fig[1,1], show_axis=false);
+    
+    if interactive
+        transf = GeoMakie.Geodesy.ECEFfromLLA(GeoMakie.Geodesy.WGS84())
+        ax = LScene(fig[1,1], show_axis=false);
+    else
+        ax = GeoAxis(fig[1, 1],
+            title= title,
+            dest = "+proj=ortho +lon_0=30 +lat_0=45")
+    end
 
     faces = get_faces(grid)
     polygons = [Polygon(faces[:, ij]) for ij in axes(faces, 2)]
     p = poly!(ax, polygons, color=grid.data; colormap)
-    p.transformation.transform_func[] = transf
+    interactive && (p.transformation.transform_func[] = transf)
 
     if coastlines
         c = lines!(GeoMakie.coastlines(50); color=:white, linewidth=1, alpha=0.7)
-        c.transformation.transform_func[] = transf
+        interactive && (c.transformation.transform_func[] = transf)
     end
 
     # Makie stuff
-    cc = cameracontrols(ax.scene)
-    cc.settings.mouse_translationspeed[] = 0.0
-    cc.settings.zoom_shift_lookat[] = false
-    Makie.update_cam!(ax.scene, cc)
+    if interactive
+        cc = cameracontrols(ax.scene)
+        cc.settings.mouse_translationspeed[] = 0.0
+        cc.settings.zoom_shift_lookat[] = false
+        Makie.update_cam!(ax.scene, cc)
+    else
+        hidedecorations!(ax)
+    end
 
     return fig
 end
