f4e99b0b19a58c5d40dbf911647b7180ae9f5bbe
milankl <milankloewer@gmx.de>
2024-11-25 16:00:27 +0000

    WIP: lm_range via ij2k

--- ext/SpeedyWeatherCUDAExt/legendre.jl
+++ ext/SpeedyWeatherCUDAExt/legendre.jl
@@ -1,12 +1,9 @@
-# Convenience functions for calculating lm and lm_end from a given m within a 
-# Kernel
-function get_lm_end(m, lmax)
-    return Int((m*lmax) - ((m-2)*m) + 0.5*m*(m-1))
-end
+# convert i, j indices of a matrix (here 0-based l,m though...) to a single 1-based running index
+import LowerTriangularMatrices: ij2k
 
-function get_lm(m, lmax)
-    return get_lm_end(m-1, lmax) + 1
-end
+# range of the running indices lm in a l-column (degrees of spherical harmonics)
+# given the column index m (order of harmonics) 
+get_lm_range(m, lmax) = ij2k(m, m, lmax):ij2k(lmax, m, lmax)
 
 # (inverse) legendre transform kernel, called from _legendre!
 function phase_factor_kernel!(
@@ -23,20 +20,19 @@ function phase_factor_kernel!(
     if tid <= length(kjm_indices)
         k, j, m  = kjm_indices[tid]
 
-        lm = get_lm(m, lmax)
-        lm_end = lm + lmax-m+1          # last index in column
+        # are m, lmax 0-based here or 1-based? 
+        lm_range = get_lm_range(m, lmax)    # assumes 1-based
 
         # view on lower triangular column, but batched in vertical
-        spec_view = view(specs_data, lm:lm_end, :)
-        legendre_view = view(legendre_polynomials_data, lm:lm_end, j)
+        spec_view = view(specs_data, lm_range, :)
+        legendre_view = view(legendre_polynomials_data, lm_range, j)
 
 
         # dot product but split into even and odd harmonics on the fly as this 
         # is how the previous implementation was enacted
-
-        lmax_range, _ = axes(spec_view)             # lmax is the number of degrees at order m, 
-        isoddlmax = isodd(length(lmax_range))
-        lmax_even = length(lmax_range) - isoddlmax    # if lmax is odd do last odd element after the loop
+        lmax_range = length(lm_range)           # number of degrees at order m, lmax-m
+        isoddlmax = isodd(lmax_range)
+        lmax_even = lmax_range - isodddlmax     # if odd do last odd element after the loop
 
         # Got rid of bounds check, potentially unsafe?
         # @boundscheck size(north) == size(south) || throw(DimensionMismatch)
@@ -75,19 +71,19 @@ Inverse Legendre transform, adapted for CUDA and batched across j (lattitude),
 k (vertical layers) and m (spherical harmonic order). Not to be used directly, 
 but called from transform! with CuArrays."""
 function SpeedyTransforms._legendre!(
-    g_north::CuArray{<:Complex, 3},   # Legendre-transformed output, northern latitudes
-    g_south::CuArray{<:Complex, 3},   # and southern latitudes
-    specs::LowerTriangularArray,            # input: spherical harmonic coefficients
-    S::SpectralTransform,                   # precomputed transform
-    kjm_indices::CuArray;            # precomputed jm index map
-    unscale_coslat::Bool = false,           # unscale by cosine of latitude on the fly?
+    g_north::CuArray{<:Complex, 3},     # Legendre-transformed output, northern latitudes
+    g_south::CuArray{<:Complex, 3},     # and southern latitudes
+    specs::LowerTriangularArray,        # input: spherical harmonic coefficients
+    S::SpectralTransform,               # precomputed transform
+    kjm_indices::CuArray;               # precomputed jm index map
+    unscale_coslat::Bool = false,       # unscale by cosine of latitude on the fly?
 )
-    (; nlat_half) = S                       # dimensions    
-    (; lmax, mmax ) = S                     # 0-based max degree l, order m of spherical harmonics  
-    (; legendre_polynomials) = S            # precomputed Legendre polynomials    
-    (; mmax_truncation) = S                 # Legendre shortcut, shortens loop over m, 0-based  
+    (; nlat_half) = S                   # dimensions    
+    (; lmax, mmax ) = S                 # 0-based max degree l, order m of spherical harmonics  
+    (; legendre_polynomials) = S        # precomputed Legendre polynomials    
+    (; mmax_truncation) = S             # Legendre shortcut, shortens loop over m, 0-based  
     (; coslat⁻¹, lon_offsets ) = S
-    nlayers = axes(specs, 2)                # get number of layers of specs for fewer layers than precomputed in S
+    nlayers = axes(specs, 2)            # get number of layers of specs for fewer layers than precomputed in S
 
     @boundscheck SpeedyTransforms.ismatching(S, specs) || throw(DimensionMismatch(S, specs))
     @boundscheck size(g_north) == size(g_south) == (S.nfreq_max, S.nlayers, nlat_half) || throw(DimensionMismatch(S, specs))
@@ -96,7 +92,7 @@ function SpeedyTransforms._legendre!(
     g_south .= 0
 
     # INVERSE LEGENDRE TRANSFORM by looping over wavenumbers l, m and layer k
-    k = CUDA.@cuda launch=false phase_factor_kernel!(
+    kernel = CUDA.@cuda launch=false phase_factor_kernel!(
         g_north,
         g_south,
         specs.data,
@@ -108,7 +104,9 @@ function SpeedyTransforms._legendre!(
     config = CUDA.launch_configuration(k.fun)
     threads = min(length(kjm_indices), config.threads)
     blocks = cld(length(kjm_indices), threads)
-    k(
+
+    # actually launch kernel!
+    kernel(
         g_north,
         g_south,
         specs.data,
