055bfbec37efe7e251f960814e11a38fe36f8e1b
milankl <milankloewer@gmx.de>
2024-12-12 01:19:46 +0000

    tests adapted

--- test/optical_depth.jl
+++ test/optical_depth.jl
@@ -3,20 +3,20 @@
 
     for OD in (ZeroOpticalDepth, FriersonOpticalDepth)
         optical_depth = OD(spectral_grid)
-        longwave_radiation = OneBandRadiation(spectral_grid)
+        longwave_radiation = NBandRadiation(spectral_grid)
         
         model = PrimitiveWetModel(spectral_grid; optical_depth, longwave_radiation)
         simulation = initialize!(model)
         run!(simulation, period=Day(1))
     
         band = longwave_radiation.nbands
-        τ = simulation.diagnostic_variables.column.optical_depth_longwave[:, band]
+        dτ = simulation.diagnostic_variables.column.optical_depth_longwave[:, band]
 
         (; nlayers) = spectral_grid
 
         for k in 2:nlayers
-            # optical depth has to increase towards the surface
-            @test τ[k] >= τ[k-1]
+            # optical depth cannot be negative
+            @test τ[k] >= 0
         end
     end
 end
