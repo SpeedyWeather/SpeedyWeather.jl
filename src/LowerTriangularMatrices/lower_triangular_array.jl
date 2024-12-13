c24a58b423385ee1c9cfd3899cd19c2052543340
Max <maximilian.gelbrecht@posteo.de>
2024-12-13 16:47:08 +0100

    backend instead of get_backend

--- src/LowerTriangularMatrices/lower_triangular_array.jl
+++ src/LowerTriangularMatrices/lower_triangular_array.jl
@@ -630,10 +630,10 @@ function Base.similar(
     return LowerTriangularArray{T, N, ArrayType{T,N}}(undef, size(L; as=Matrix))
 end
 
-function GPUArrays.get_backend(
+function GPUArrays.backend(
     ::Type{LowerTriangularArray{T, N, ArrayType}}
 ) where {T, N, ArrayType <: GPUArrays.AbstractGPUArray}
-    return GPUArrays.get_backend(ArrayType)
+    return GPUArrays.backend(ArrayType)
 end
 
 Adapt.adapt_structure(to, L::LowerTriangularArray) =
