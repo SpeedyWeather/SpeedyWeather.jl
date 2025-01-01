9a436e66ab07d91f5d06c8cfd39369c37a27f0ef
milankl <milankloewer@gmx.de>
2025-01-02 00:22:21 +0100

    tracer doc strings

--- src/dynamics/tracers.jl
+++ src/dynamics/tracers.jl
@@ -1,15 +1,22 @@
 abstract type AbstractTracer <: AbstractModelComponent end
 
 export Tracer
+
+"""Define tracer through a `name::Symbol` (used as key in the tracer dictionaries)
+and define it as `active` (=true default). `active=false` will (temporarily)
+disable the time evolution of the tracer. Fields are
+$(TYPEDFIELDS)"""
 @kwdef mutable struct Tracer <: AbstractTracer
     name::Symbol
     active::Bool = true
 end
 
+# method to create a tracer from a positional symbol
 Tracer(name::Symbol; kwargs...) = Tracer(; name, kwargs...)
 
 const TRACER_DICT = Dict{Symbol, AbstractTracer}
 
+# low-level function to add tracers to the tracer dictionary
 function add!(dict::TRACER_DICT, tracers::Tracer...)
     for tracer in tracers
         dict[tracer.name] = tracer
@@ -17,7 +24,10 @@ function add!(dict::TRACER_DICT, tracers::Tracer...)
     return dict
 end
 
+# calls the above as `model.tracers` is a `TRACER_DICT`
 add!(model::AbstractModel, tracers::Tracer...) = add!(model.tracers, tracers...)
+
+# adding a tracer to a simulation adds it to the model and the prognostic and diagnostic variables
 function add!(simulation::AbstractSimulation, tracers::Tracer...)
     add!(simulation.model, tracers...)                      # first add tracer to model
     tracers_tuple = values(simulation.model.tracers)        # get all tracers from there
@@ -29,8 +39,14 @@ end
 add!(vars::AbstractVariables, tracer_dict::TRACER_DICT) = add!(vars, values(tracer_dict)...)
 
 export activate!
+
+"""$(TYPEDSIGNATURES) Activate a deactivated (=frozen) tracers in a simulation, which is a setting in `simulation.model` only."""
 activate!(simulation::AbstractSimulation, tracers::Tracer...) = activate!(simulation.model, tracers...)
+
+# propagate activate! from simulation to model to the `model.tracers` dictionary
 activate!(model::AbstractModel, tracers::Tracer...) = activate!(model.tracers, tracers...)
+
+# pass on "value" to also allow _activate! to be used for decativation
 activate!(dict::TRACER_DICT, tracers::Tracer...) = _activate!(dict, tracers..., value=true)
 function _activate!(dict::TRACER_DICT, tracers::Tracer...; value::Bool=true)
     for tracer in tracers
@@ -39,14 +55,20 @@ function _activate!(dict::TRACER_DICT, tracers::Tracer...; value::Bool=true)
 end
 
 export deactivate!
+
+"""$(TYPEDSIGNATURES) Deactivate a tracer in a simulation, which is a setting in `simulation.model` only."""
 deactivate!(simulation::AbstractSimulation, tracers::Tracer...) = deactivate!(simulation.model, tracers...)
 deactivate!(model::AbstractModel, tracers::Tracer...) = deactivate!(model.tracers, tracers...)
 deactivate!(dict::TRACER_DICT, tracers::Tracer...) = _activate!(dict, tracers..., value=false)
 
-Base.delete!(model::AbstractModel, tracer::Tracer) = delete!(model.tracers, tracer.name)
+
+"""$(TYPEDSIGNATURES) Delete a tracer from a simulation, deleted from the model and the variables."""
 function Base.delete!(simulation::AbstractSimulation, tracer::Tracer)
     delete!(simulation.model, tracer)
     delete!(simulation.prognostic_variables, tracer)
     delete!(simulation.diagnostic_variables, tracer)
     simulation.model.tracers
 end
+
+# delete from tracer dictionary, identified by its `name::Symbol`
+Base.delete!(model::AbstractModel, tracer::Tracer) = delete!(model.tracers, tracer.name)
