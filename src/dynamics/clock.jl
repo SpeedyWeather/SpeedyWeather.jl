71df88bc861a21c3641729e251fd8ad1074540f3
Milan Klöwer <milankloewer@gmx.de>
2025-03-05 16:36:41 +0000

    run! with steps argument (#684)
    * run! with steps argument
    
    * add tests
    
    * changelog updated
    
    * initialize clock with either n_timesteps or period
    
    * ::AbstractTimeStepper arg as Leapfrog not defined yet
    
    * export Millisecond
    
    * test that steps and period cannot be set simultaneously in run!
    
    * update dates.jl tests
    
    * update schedule tests with no set_period! function anymore

--- src/dynamics/clock.jl
+++ src/dynamics/clock.jl
@@ -1,18 +1,20 @@
 const DEFAULT_DATE = DateTime(2000, 1, 1)
 
+abstract type AbstractClock end
+
 export Clock
 """
 Clock struct keeps track of the model time, how many days to integrate for
 and how many time steps this takes
 $(TYPEDFIELDS)."""
-Base.@kwdef mutable struct Clock
+Base.@kwdef mutable struct Clock <: AbstractClock
     "current model time"
     time::DateTime = DEFAULT_DATE
 
     "start time of simulation"
     start::DateTime = DEFAULT_DATE
     
-    "period to integrate for, set in set_period!(::Clock, ::Dates.Period)"
+    "period to integrate for"
     period::Second = Second(0)
 
     "Counting all time steps during simulation"
@@ -50,30 +52,35 @@ function Base.copy!(clock::Clock, clock_old::Clock)
     return nothing 
 end 
 
-"""
-$(TYPEDSIGNATURES)
-Initialize the clock with the time step `Δt` in the `time_stepping`."""
-function initialize!(clock::Clock, time_stepping::AbstractTimeStepper)
-    clock.n_timesteps = ceil(Int, clock.period.value/time_stepping.Δt_sec)
+"""$(TYPEDSIGNATURES)
+Initialize the clock with the time step `Δt` from `time_stepping`."""
+initialize!(clock::Clock, time_stepping::AbstractTimeStepper, args...) =
+    initialize!(clock, time_stepping.Δt_millisec, args...)
+
+"""$(TYPEDSIGNATURES)
+Initialize the clock with the time step `Δt` and `period` to integrate for."""
+function initialize!(clock::Clock, Δt::Period, period::Period)
+    clock.Δt = Δt
+    clock.period = period
+    clock.n_timesteps = ceil(Int, Millisecond(period).value/Millisecond(Δt).value)
+    initialize!(clock)      # set start time, reset counter
+end
+
+"""$(TYPEDSIGNATURES)
+Initialize the clock with the time step `Δt` and the number of time steps `n_timesteps`."""
+function initialize!(clock::Clock, Δt::Period, n_timesteps::Integer)
+    clock.Δt = Δt
+    clock.n_timesteps = n_timesteps
+    clock.period = Δt * n_timesteps
+    initialize!(clock)
+end
+
+"""$(TYPEDSIGNATURES)
+Initialize the clock with setting the start time and resetting the timestep counter."""
+function initialize!(clock::Clock)
     clock.start = clock.time    # store the start time
     clock.timestep_counter = 0  # reset counter
-    clock.Δt = time_stepping.Δt_millisec
     return clock
-end
-
-"""
-$(TYPEDSIGNATURES)
-Set the `period` of the clock to a new value. Converts any `Dates.Period` input to `Second`."""
-function set_period!(clock::Clock, period::Period)
-    clock.period = Second(period)
-end
-
-"""
-$(TYPEDSIGNATURES)
-Set the `period` of the clock to a new value. Converts any `::Real` input to `Day`."""
-function set_period!(clock::Clock, period::Real)
-    @info "Input $period assumed to have units of days. Use Week($period), Hour($period), Minute($period) otherwise."
-    clock.period = Day(period)
 end
 
 """
@@ -81,5 +88,5 @@ $(TYPEDSIGNATURES)
 Create and initialize a clock from `time_stepping`"""
 function Clock(time_stepping::AbstractTimeStepper; kwargs...)
     clock = Clock(; kwargs...)
-    initialize!(clock, time_stepping)
+    initialize!(clock, time_stepping, clock.period)
 end
