6fac0518cfa1cfd62dbdbec3f8b5de538050b65f
Milan Kloewer <milankloewer@gmx.de>
2025-04-28 08:49:40 +0200

    Speed up CI for land

--- test/physics/land.jl
+++ test/physics/land.jl
@@ -29,7 +29,13 @@ end
     
                     model = Model(spectral_grid; land)
                     simulation = initialize!(model)
-                    run!(simulation, period=Hour(2))
+                    
+                    progn = simulation.prognostic_variables
+                    diagn = simulation.diagnostic_variables
+                    land_timestep!(progn, diagn, model)
+
+                    # don't run full model as this recompiles for every iteration of the loop
+                    # run!(simulation, period=Hour(2))
                 end
             end
         end
