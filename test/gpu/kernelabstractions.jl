2c0d70c1b4e305fe098f21877de36331aedc8366
Max <maximilian.gelbrecht@posteo.de>
2025-05-07 18:43:04 +0200

    delete old KA test

--- test/gpu/kernelabstractions.jl
+++ test/gpu/kernelabstractions.jl
@@ -2,21 +2,7 @@ using KernelAbstractions
 import SpeedyWeather: on_architecture
 @testset "KernelAbstractions tests" begin 
 
-    # only on CPU (currently)
-    arch = SpeedyWeather.CPU()
-
-    @kernel function mul_test!(A, @Const(B), @Const(C))
-        i, j = @index(Global, NTuple)
-        A[i, j] = B[i, j] * C[i, j]
-    end
-    
-    B = on_architecture(arch, rand(10, 10))
-    C = on_architecture(arch, rand(10, 10))
-    A = zero(B)
-
-    SpeedyWeather.launch!(arch, typeof(A), size(B), mul_test!, A, B, C)
-
-    @test A ≈ B .* C 
+    # To-Do write tests for each type of dims_type in the kernel launching util
 
     NF= Float32
     alms = rand(LowerTriangularArray{Complex{NF}},33, 32, 2)
