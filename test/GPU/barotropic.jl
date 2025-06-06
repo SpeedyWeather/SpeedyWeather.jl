b4404891e97cfc4b3c941d2b6eed8d90abd1439b
Max <maximilian.gelbrecht@posteo.de>
2025-06-06 20:10:33 +0200

    allowscalar in init in test

--- test/GPU/barotropic.jl
+++ test/GPU/barotropic.jl
@@ -1,7 +1,7 @@
 @testset "GPU Barotropic" begin
     spectral_grid = SpectralGrid(trunc=32, architecture=SpeedyWeather.GPU())
     model = BarotropicModel(spectral_grid=spectral_grid)
-    simulation = initialize!(model)
+    CUDA.@allowscalar simulation = initialize!(model)
     run!(simulation, steps=4)
 
     @test simulation.model.feedback.nars_detected == false
