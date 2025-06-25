5820ac83510caed8627f83f041e65fffef907e09
Brian Groenke <brian.groenke@pik-potsdam.de>
2025-06-25 20:43:59 +0200

    Generalize parameters type arg and add tests

--- test/parameters.jl
+++ test/parameters.jl
@@ -1,6 +1,7 @@
+using ComponentArrays: ComponentVector
+using DomainSets: Domain, RealLine, UnitInterval
 using ModelParameters
 using SpeedyWeather: value, bounds, description, attributes, parameters, reconstruct
-using DomainSets: Domain, RealLine
 
 @testset "SpeedyParam" begin
     # Test basic construction and getter functions
@@ -19,11 +20,19 @@ end
 
 @testset "parameters" begin
     # check basic function of parameters method
+    @test parameters(1.0) == SpeedyParam(1.0)
+    @test parameters(Param, 1.0) == Param(1.0)
+    @test parameters(SpeedyParam, 1.0, bounds=UnitInterval()) == SpeedyParam(1.0, bounds=UnitInterval())
+    # test for non-nested type
     earth = Earth(Float64)
     ps = parameters(earth, category="planet")
     @test values(stripparams(ps)) == (earth.rotation, earth.gravity, earth.axial_tilt, earth.solar_constant)
     @test all(map(p -> p.category == "planet", ps))
+    # test for nested model type
+    spectral_grid = SpectralGrid(trunc=31, nlayers=1)   # define resolution
+    model = BarotropicModel(spectral_grid)
+    model_ps = ComponentVector(parameters(model))
+    new_model = SpeedyWeather.reconstruct(model, 2*model_ps)
+    new_model_ps = ComponentVector(parameters(new_model))
+    @test all(new_model_ps .== 2*model_ps)
 end
-
-spectral_grid = SpectralGrid(trunc=31, nlayers=1)   # define resolution
-model = Barotropic(spectral_grid)
