15d1c362e7c0f79f68e613186b5306599fdf80cd
Max <maximilian.gelbrecht@posteo.de>
2025-07-02 16:25:46 +0200

    just one layer for barotropic model

--- test/GPU/barotropic.jl
+++ test/GPU/barotropic.jl
@@ -1,5 +1,5 @@
 @testset "GPU Barotropic" begin
-    spectral_grid = SpectralGrid(trunc=32, architecture=SpeedyWeather.GPU())
+    spectral_grid = SpectralGrid(trunc=32, nlayers=1, architecture=SpeedyWeather.GPU())
     model = BarotropicModel(spectral_grid=spectral_grid)
     CUDA.@allowscalar simulation = initialize!(model)
     run!(simulation, steps=4)
