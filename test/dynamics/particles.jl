802d20cfc3a0b87260370c9cc10a18e3b4697a0c
Max <maximilian.gelbrecht@posteo.de>
2025-07-04 08:20:53 +0200

    particle tests adjusted

--- test/dynamics/particles.jl
+++ test/dynamics/particles.jl
@@ -1,9 +1,9 @@
 @testset "Create and compare particles" begin
-    @test Particle(0, 0) == Particle(0, 0, 0)
-    @test Particle(0, 0, 0) == zero(Particle) == zero(Particle{Float32}) == zero(Particle{Float32, true})
-    @test Particle(0.0, 0.0) == Particle(0f0, 0f0, 0.0) == Particle(0, Float16(0))
-    @test zero(Particle{Float32}) == Particle{Float32,true}(0, 0)
-    @test zero(Particle{Float32}) != Particle{Float32,false}(0, 0)
+    @test Particle(0, 0) == Particle(0, 0, 0) == Particle(true, 0, 0, 0)
+    @test Particle(true, 0, 0, 0) == zero(Particle) == zero(Particle{Float32})
+    @test Particle(0.0, 0.0) == Particle(0f0, 0f0, 0.0) == Particle(true, 0, 0, 0)
+    @test zero(Particle{Float32}) == Particle{Float32}(true, 0, 0, 0)
+    @test zero(Particle{Float32}) != Particle{Float32}(false,0, 0)
     @test deactivate(zero(Particle)) != zero(Particle)
     @test activate(zero(Particle)) == zero(Particle)
 
@@ -16,7 +16,7 @@
     @test Particle{Float16}(lon=1,lat=2,σ=3.0) == Particle{Float32}(1,2,3)
     @test active(Particle(1,2,3))
     @test ~active(deactivate(Particle(1,2,3)))
-    @test ~active(Particle{Float64,false}(1,2,3))
+    @test ~active(Particle{Float64}(false,1,2,3))
     @test Particle(lon=0,lat=90) == Particle(lon=10,lat=90)
     @test Particle(lon=0,lat=-90) == Particle(lon=10,lat=-90)
     @test Particle(lon=0,lat=-90) != Particle(lon=10,lat=90)
@@ -72,11 +72,9 @@ end
 @testset "Random particles" begin
     @test rand(Particle) isa Particle
     @test rand(Particle{Float16}) isa Particle
-    @test rand(Particle{Float64,false}) isa Particle
 
     @test rand(Particle,5) isa Vector{Particle}
     @test rand(Particle{Float32},5) isa Vector{Particle{Float32}}
-    @test rand(Particle{Float32,false},5) isa Vector{Particle{Float32,false}}
 
     for NF in (Float16, Float32, Float64)
         for i in 1:1000
@@ -90,7 +88,6 @@ end
         v = zeros(Particle{NF},5)       # active/inactive not specified
         v[1] = Particle(lon=1, lat=2)
         v[2] = Particle{Float64}(lon=1, lat=2)
-        v[3] = Particle{Float64, false}(lon=1, lat=2)
 
         for particle in v
             @test particle isa Particle{NF}
@@ -101,7 +98,6 @@ end
         v = zeros(Particle{NF, true}, 5)                # all particles active
         v[1] = Particle(lon=1, lat=2)
         v[2] = Particle{Float64}(lon=1, lat=2)
-        v[3] = Particle{Float64, false}(lon=1, lat=2)   # will convert to active
 
         for particle in v
             @test particle isa Particle{NF, true}
