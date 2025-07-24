b60896b60b2ed7ddf351b2dffd30676d50aed40b
Maximilian Gelbrecht <maxgelbr@pik-potsdam.de>
2025-07-24 11:08:27 +0200

    primitive dry test with arch

--- test/GPU/primitive_dry.jl
+++ test/GPU/primitive_dry.jl
@@ -1,5 +1,6 @@
 @testset "GPU PrimitiveDryModel" begin
-    spectral_grid = SpectralGrid(trunc=32, nlayers=8, architecture=SpeedyWeather.GPU())
+    arch = SpeedyWeather.GPU()
+    spectral_grid = SpectralGrid(trunc=32, nlayers=8, architecture=arch)
     model = PrimitiveDryModel(spectral_grid=spectral_grid, physics=false)
     simulation = CUDA.@allowscalar initialize!(model)
     run!(simulation, steps=4)
