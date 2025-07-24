c27ff3ce36586baa9b0ed20ca52c3a20a23b82de
Max <maximilian.gelbrecht@posteo.de>
2025-07-24 08:41:17 +0200

    restrict test to physics false

--- test/GPU/primitive_dry.jl
+++ test/GPU/primitive_dry.jl
@@ -1,6 +1,6 @@
 @testset "GPU PrimitiveDryModel" begin
     spectral_grid = SpectralGrid(trunc=32, nlayers=8, architecture=SpeedyWeather.GPU())
-    model = PrimitiveDryModel(spectral_grid=spectral_grid)
+    model = PrimitiveDryModel(spectral_grid=spectral_grid, physics=false)
     simulation = CUDA.@allowscalar initialize!(model)
     run!(simulation, steps=4)
 
