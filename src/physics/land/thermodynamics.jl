83e5ef7af3b3d45595fbe2e6b305e5135dde1551
Pier Luigi Vidale <108409300+pierluigividale@users.noreply.github.com>
2025-07-31 12:42:56 +0100

    Update thermodynamics.jl
    Merged with what Milan and I did on 30 July

--- src/physics/land/thermodynamics.jl
+++ src/physics/land/thermodynamics.jl
@@ -8,17 +8,17 @@ export LandThermodynamics
     "[OPTION] Field capacity γ per meter soil [1]"
     field_capacity::NF = 0.24
 
+    "[OPTION] Field capacity (W_cap or γ) [volume fraction]"
+    field_capacity::NF = 0.3
+
     "[OPTION] Heat capacity Cw of water [J/(m³ K)]"
     heat_capacity_water::NF = 4.2e6
 
     "[OPTION] Heat capacity Cs of dry soil [J/(m³ K)]"
     heat_capacity_dry_soil::NF = 1.13e6
 
-    "[OPTION] Soil wetness at field capacity [volume fraction]"
-    W_cap::NF = 0.3
-
     "[OPTION] Soil wetness at wilting point [volume fraction]"
-    W_wilt::NF = 0.17
+    wilting_point::NF = 0.17
 end
 
 LandThermodynamics(SG::SpectralGrid; kwargs...) = LandThermodynamics{SG.NF}(; kwargs...)
