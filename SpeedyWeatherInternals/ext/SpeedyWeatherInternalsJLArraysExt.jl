d1a90ccf20b7391087c912d97869a9a08fd11ee6
Max <maximilian.gelbrecht@posteo.de>
2025-09-12 22:21:32 +0200

    on_architecture for JLArrays

--- SpeedyWeatherInternals/ext/SpeedyWeatherInternalsJLArraysExt.jl
+++ SpeedyWeatherInternals/ext/SpeedyWeatherInternalsJLArraysExt.jl
@@ -9,6 +9,17 @@ module SpeedyWeatherInternalsJLArraysExt
     Architectures.ismatching(arch::GPU, array_type::Type{<:JLArrays.JLDeviceArray}) = true
     Architectures.ismatching(arch::Type{<:GPU}, array_type::Type{<:JLArrays.JLDeviceArray}) = true
     Architectures.architecture(::Type{<:JLArray}) = GPU(JLArrays.JLBackend())
+
+    Architectures.on_architecture(::CPU, a::JLArray) = Array(a)
+    Architectures.on_architecture(::GPU{JLBackend}, a::JLArray) = a
+    Architectures.on_architecture(::CPU, a::SubArray{<:Any, <:Any, <:JLArray}) = Array(a)
+
+    Architectures.on_architecture(::GPU{JLBackend}, a::Array) = JLArray(a)
+    Architectures.on_architecture(::GPU{JLBackend}, a::BitArray) = JLArray(a)
+    Architectures.on_architecture(::GPU{JLBackend}, a::SubArray{<:Any, <:Any, <:Array}) = JLArray(a)
+    Architectures.on_architecture(::GPU{JLBackend}, a::SubArray{<:Any, <:Any, <:JLArray}) = a
+    Architectures.on_architecture(::GPU{JLBackend}, a::StepRangeLen) = a
+    
     Architectures.array_type(::Type{GPU{JLBackend}}) = JLArray
     Architectures.array_type(::GPU{JLBackend}) = JLArray
     Architectures.array_type(::GPU{JLBackend}, NF::Type, N::Int) = JLArray{NF, N}
