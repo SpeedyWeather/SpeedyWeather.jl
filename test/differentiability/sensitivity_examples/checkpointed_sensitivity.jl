a95a7fae0dcc3f56f6cba624c8eb1813c43342f1
Max <maximilian.gelbrecht@posteo.de>
2025-10-15 17:14:51 +0200

    update checkpointed sens

--- test/differentiability/sensitivity_examples/checkpointed_sensitivity.jl
+++ test/differentiability/sensitivity_examples/checkpointed_sensitivity.jl
@@ -4,7 +4,7 @@ Pkg.activate("test/differentiability/sensitivity_examples")
 using SpeedyWeather, Enzyme, JLD2, Checkpointing
 
 # Parse command line argument for N (number of timesteps)
-const N = length(ARGS) >= 1 ? parse(Int, ARGS[1]) : 2
+const N = length(ARGS) >= 1 ? parse(Int, ARGS[1]) : 100
 
 println("Running Sensitivity Analyis with N = $N")
 savename_base = "sensitivity-$N"
