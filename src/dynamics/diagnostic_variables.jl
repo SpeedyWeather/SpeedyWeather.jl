581bd99080fe6d5ad0672f607de92ebe18e3da43
Milan Kloewer <milankloewer@gmx.de>
2025-11-10 14:27:33 +0000

    create DiagnosticVariables from spectral_grid, ScratchMemory bug

--- src/dynamics/diagnostic_variables.jl
+++ src/dynamics/diagnostic_variables.jl
@@ -246,29 +246,24 @@ end
 Generator function. If a `spectral_transform` is handed over, the same scratch memory is used."""
 function DynamicsVariables(SG::SpectralGrid; 
                            spectral_transform::Union{Nothing,SpectralTransform}=nothing) 
-    (; architecture, spectrum, grid, nlayers, NF, ArrayType) = SG
+    
+    (; spectrum, grid, nlayers, NF, ArrayType) = SG
     (; SpectralVariable2D, SpectralVariable3D) = SG
     (; GridVariable2D, GridVariable3D) = SG
 
-    if isnothing(spectral_transform)
-        return DynamicsVariables{NF, ArrayType, typeof(spectrum), typeof(grid),
-            SpectralVariable2D, SpectralVariable3D,
-            GridVariable2D, GridVariable3D, 
-            SpeedyTransforms.ScratchMemory{NF, array_type(architecture, Complex{NF}, 3), 
-            array_type(architecture, NF, 1), array_type(architecture, Complex{NF}, 1)}}(;
-                spectrum, grid, nlayers,
-            )
-    else 
-        scratch_memory = spectral_transform.scratch_memory 
 
-        return DynamicsVariables{NF, ArrayType, typeof(spectrum), typeof(grid),
-            SpectralVariable2D, SpectralVariable3D,
-            GridVariable2D, GridVariable3D, typeof(scratch_memory)}(;
-                spectrum, grid, nlayers, scratch_memory
-            )
-    end 
+    if isnothing(spectral_transform)    # then create ScratchMemory now
+        scratch_memory = SpeedyTransforms.ScratchMemory(NF, ArrayType, grid, nlayers)
+    else                                # otherwise reuse existing ScratchMemory
+        scratch_memory = spectral_transform.scratch_memory
+    end
+
+    return DynamicsVariables{NF, ArrayType, typeof(spectrum), typeof(grid),
+        SpectralVariable2D, SpectralVariable3D,
+        GridVariable2D, GridVariable3D, typeof(scratch_memory)}(;
+            spectrum, grid, nlayers, scratch_memory
+        )
 end
-
 
 export DynamicsVariablesOcean
 @kwdef struct DynamicsVariablesOcean{
