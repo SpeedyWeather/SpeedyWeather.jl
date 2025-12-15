f1c3a19c817516363f2b758dc8db19ad671c3019
Max <maximilian.gelbrecht@posteo.de>
2025-12-15 15:23:51 +0100

    benchmark update

--- benchmark/CUDA/define_benchmarks.jl
+++ benchmark/CUDA/define_benchmarks.jl
@@ -19,3 +19,21 @@ benchmarks[:benchmark101] = BenchmarkSuiteTransform(
     NF = fill(Float32, 3),
     model = fill(GPU(), 3),
 )
+
+benchmarks[:benchmark200] = BenchmarkSuiteModel(
+    title = "PrimitiveWet benchmarks, CPU",
+    nruns = 3,
+    trunc = [31, 63, 127, 255, 511],
+    nlayers = fill(16, 5),
+    Grid = fill(SpeedyWeather.DEFAULT_GRID, 5),
+    architecture = fill(CPU(), 5),
+)
+
+benchmarks[:benchmark200] = BenchmarkSuiteModel(
+    title = "PrimitiveWet benchmarks, GPU",
+    nruns = 3,
+    trunc = [31, 63, 127, 255, 511],
+    nlayers = fill(16, 5),
+    Grid = fill(SpeedyWeather.DEFAULT_GRID, 5),
+    architecture = fill(GPU(), 5),
+)
