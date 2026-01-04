5b7f006fdcdaf3093e79b5e49322ee102701274f
Milan Kloewer <milankloewer@gmx.de>
2026-01-04 12:29:17 +0000

    do makie tests last

--- RingGrids/test/runtests.jl
+++ RingGrids/test/runtests.jl
@@ -5,7 +5,10 @@ include("column_field.jl")
 include("geodesics.jl")
 include("grids.jl")
 include("interpolation.jl")
-include("makie.jl")
 include("reordering.jl")
 include("reverse.jl")
 include("rotate.jl")
+
+# must be last due to using CairoMakie and GeoMakie
+# which export rotate! too
+include("makie.jl")
