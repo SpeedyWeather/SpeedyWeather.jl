8ec9010ee173d783cffd382573f001472d0835b3
Maximilian Gelbrecht <maxgelbr@pik-potsdam.de>
2026-02-02 18:15:56 +0100

    inverse transfrom non-split default again

--- SpeedyTransforms/src/matrix_transform.jl
+++ SpeedyTransforms/src/matrix_transform.jl
@@ -173,7 +173,7 @@ coefficients to an n-dimensional array `field`. Uses precomputed dense transform
 the transformation. The spectral transform is number format-flexible but `field` and the spectral
 transform `M` have to have the same number format. The spectral transform is grid-flexible as long
 as `field.grid` and `M.grid` match."""
-function transform_old!(                        # SPECTRAL TO GRID
+function transform!(                        # SPECTRAL TO GRID
         field::AbstractField,               # gridded output
         coeffs::LowerTriangularArray,       # spectral coefficients input
         scratch_memory,                     # explicit scratch memory to use
@@ -186,29 +186,24 @@ function transform_old!(                        # SPECTRAL TO GRID
     @boundscheck ismatching(M, coeffs) || throw(DimensionMismatch(M, coeffs))
 
     nlayers = size(coeffs, 2)
-    if nlayers < size(scratch_memory, 2)
-        # use first n layers of scratch memory
-        scratch = view(scratch_memory, :, 1:nlayers)
 
-        # multiply into scratch which is complex typed and then take real part into field
-        # imaginary part should be zero but destination is used to store intermediate results
-        # explicitly convert to real also for NaN + NaN*im results
-        LinearAlgebra.mul!(scratch, M.backward, coeffs.data)
-        field.data .= real.(scratch)
+    # use first n layers of scratch memory if nlayers is less than the number of layers in scratch_memory
+    scratch = ndims(coeffs) == 1 ? view(scratch_memory, :, 1) : nlayers < size(scratch_memory, 2) ? view(scratch_memory, :, 1:nlayers) : scratch_memory
 
-    else    # don't use view for 3D transforms with all layers
-        # TODO if multiplication yields all real then one could write directly into field.data
-        # which would be much faster but if the imaginary part is non-zero this throws an error
-        # so for now use the scratch memory as intermediate storage and then copy real part
-        LinearAlgebra.mul!(scratch_memory, M.backward, coeffs.data)
-        field.data .= real.(scratch_memory)
-    end
+    # multiply into scratch which is complex typed and then take real part into field
+    # imaginary part should be zero but destination is used to store intermediate results
+    # explicitly convert to real also for NaN + NaN*im results
+    # TODO if multiplication yields all real then one could write directly into field.data
+    # which would be much faster but if the imaginary part is non-zero this throws an error
+    # so for now use the scratch memory as intermediate storage and then copy real part
+    LinearAlgebra.mul!(scratch, M.backward, coeffs.data)
+    field.data .= real.(scratch)
 
     unscale_coslat && RingGrids._scale_lat!(field, M.coslat⁻¹)
     return field
 end
 
-function transform!(                        # SPECTRAL TO GRID
+function transform_split!(                        # SPECTRAL TO GRID
         field::AbstractField,               # gridded output
         coeffs::LowerTriangularArray,       # spectral coefficients input
         scratch_memory,                     # explicit scratch memory to use
