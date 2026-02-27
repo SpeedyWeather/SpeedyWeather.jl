a4a8659038c9179e159d65534de4d5a708cdf2c3
Milan Kloewer <milankloewer@gmx.de>
2026-02-27 09:15:01 +0000

    docstrings

--- SpeedyWeather/src/physics/radiation/greenhouse_gases.jl
+++ SpeedyWeather/src/physics/radiation/greenhouse_gases.jl
@@ -1,8 +1,15 @@
 abstract type AbstractGreenhouseGas <: AbstractModelComponent end
+abstract type AbstractCO2 <: AbstractGreenhouseGas end
+# variables(::AbstractCO2) = PrognosticVariable(:co2, ...)
 
-DEFAULT_CO2 = 280 # ppm
+const DEFAULT_CO2 = 280     # preindustrial CO2 [ppm]
+
 export CO2
-@kwdef struct CO2{F} <: AbstractGreenhouseGas
+"""$(TYPEDSIGNATURES) CO2 concentration [ppm] that can be a constant, e.g. `CO2(420)`
+or a function of `t::DateTime`, e.g. `CO2(t -> 280 + year(t) - 1850)` for a linear
+(but stepwise) increase by 1ppm per year."""
+@kwdef struct CO2{F} <: AbstractCO2
+    "[OPTION] CO2 concentration"
     concentration::F
 end
 
@@ -13,23 +20,41 @@ CO2(SG::SpectralGrid, concentration::Function) = CO2(concentration)
 (C::CO2{<:Function})(t::DateTime) = C.concentration(t)
 
 export TwoTimesCO2, FourTimesCO2
-TwoTimesCO2() = CO2(t -> t > DateTime(2000) ? 2DEFAULT_CO2 : DEFAULT_CO2)
-FourTimesCO2() = CO2(t -> t > DateTime(2000) ? 4DEFAULT_CO2 : DEFAULT_CO2)
+"$(TYPEDSIGNATURES) 2xCO2 scenario: Doubles preindustrial `DEFAULT_CO2` in the year 2000."
+TwoTimesCO2(t0 = DateTime(2000)) = CO2(t -> t > t0 ? 2DEFAULT_CO2 : DEFAULT_CO2)
+
+"$(TYPEDSIGNATURES) 4xCO2 scenario: Quadruples preindustrial `DEFAULT_CO2` in the year 2000."
+FourTimesCO2(t0 = DateTime(2000)) = CO2(t -> t > t0 ? 4DEFAULT_CO2 : DEFAULT_CO2)
 
 export ExponentialCO2
-@kwdef struct ExponentialCO2{NF} <: AbstractGreenhouseGas
+
+"""$(TYPEDSIGNATURES) Exponential CO2 concentration with defaults fitted
+to the Keeling curve, ignoring seasonal variation."""
+@kwdef struct ExponentialCO2{NF} <: AbstractCO2
+    "[OPTION] preindustrial CO2 concentration [ppm]"
     base_concentration::NF = 280
+
+    "[OPTION] time of preindustrial CO2"
     start::DateTime = DateTime(1850)
-    a::NF = 3.6
-    b::NF = 1/47
+
+    "[OPTION] Scaling `a` in `c + a*exp((t-t0)*b)` [ppm]"
+    a::NF = 3.85
+
+    "[OPTION] e-folding frequency b in `c + a*exp((t-t0)*b)` [1/year]"
+    b::NF = 1/48
 end
 
 ExponentialCO2(SG::SpectralGrid; kwargs...) = ExponentialCO2{SG.NF}(; kwargs...)
 function (C::ExponentialCO2{NF})(t::DateTime) where NF
 
-    # time since start in seconds
-    Δt = Dates.datetime2unix(t) - Dates.datetime2unix(C.start)
-    Δt /= (365.25f0 * 24 * 3600) # time in years
+    # time since start in seconds, convert from Float64 to NF
+    Δt = convert(NF, Dates.datetime2unix(t) - Dates.datetime2unix(C.start))
+    Δt /= (365.25f0 * 24 * 3600)    # time in years
+    Δt = convert(NF, Δt)            # to compute the exponential in NF
     
     return C.base_concentration + C.a * exp(C.b * Δt)
 end
+
+# for gas in keys(model.greenhouse_gases)
+#     simulation.variables[gas][] = model.greenhouse_gases[gas](time)
+# end
