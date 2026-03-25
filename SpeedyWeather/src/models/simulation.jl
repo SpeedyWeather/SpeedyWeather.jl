827c10b72b5cbc952230770c2102d5e14f82a3c2
Max <maximilian.gelbrecht@posteo.de>
2026-03-25 14:21:12 +0100

    simulation check not commented out anymore

--- SpeedyWeather/src/models/simulation.jl
+++ SpeedyWeather/src/models/simulation.jl
@@ -87,18 +87,15 @@ function initialize!(
     # raise a warning if starting with leapfrog but there's zero vorticity
     vor = get_step(progn.vor, lf)
 
-    #TODO: figure out what Reactant's problem with the simple statement below is:
-    #lf == 2 && all(vor .== 0) && @warn "Vorticity is zero on 2nd leapfrog index though you use it to calculate tendencies." *
-    #    " You may wanted to continue with a leapfrog step without data for it in the 2nd step."
-    #@trace if lf == 2 && all(vor .== 0)
-    #    @warn "Vorticity is zero on 2nd leapfrog index though you use it to calculate tendencies." *
-    #        " You may wanted to continue with a leapfrog step without data for it in the 2nd step."
-    #end
+    @trace if lf == 2 && all(vor .== 0)
+        @warn "Vorticity is zero on 2nd leapfrog index though you use it to calculate tendencies." *
+            " You may wanted to continue with a leapfrog step without data for it in the 2nd step."
+    end
 
-    @maybe_jit arch transform!(diagn, progn, lf, model, initialize = true)
+    transform!(diagn, progn, lf, model, initialize = true)
     initialize!(diagn, progn.particles, progn, model)
     initialize!(model.output, model.feedback, progn, diagn, model)
-    return @maybe_jit arch initialize!(model.callbacks, progn, diagn, model)
+    return initialize!(model.callbacks, progn, diagn, model)
 end
 
 """$(TYPEDSIGNATURES)
