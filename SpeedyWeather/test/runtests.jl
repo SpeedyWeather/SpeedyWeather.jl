51fce5eb8725bdfe3854a1cdf00d02489154bf2c
Milan Kloewer <milankloewer@gmx.de>
2026-03-30 17:11:26 +0100

    Separate enzyme and extended tests

--- SpeedyWeather/test/runtests.jl
+++ SpeedyWeather/test/runtests.jl
@@ -1,68 +1,51 @@
-# using SpeedyWeather
-# using Test
+using SpeedyWeather
+using Test
 
-# FLAG_EXTENDED_TESTS = "extended_tests" in ARGS ? true : false
-# FLAG_ENZYME_TESTS = "enzyme" in ARGS ? true : false
+# GENERAL
+include("spectral_grid.jl")
+include("parameters.jl")
 
-# if FLAG_EXTENDED_TESTS
-#     @info "Running extended test suite"
-#     include("differentiability/runtests.jl")
-# end
+# GPU/KERNELABSTRACTIONS
+include("GPU/kernelabstractions.jl")
 
-# if FLAG_ENZYME_TESTS
-#     @info "Running Enzyme CI tests"
-#     include("differentiability/full_diff_CI.jl")
-# end
+# DYNAMICS
+include("dynamics/horizontal_diffusion.jl")
+include("dynamics/time_stepping.jl")
+include("dynamics/vertical_advection.jl")
+include("dynamics/particles.jl")
+include("dynamics/particle_advection.jl")
+include("dynamics/forcing_drag.jl")
+include("dynamics/dates.jl")
+include("dynamics/set.jl")
+include("dynamics/copy_variables.jl")
+include("dynamics/orography.jl")
+include("dynamics/initial_conditions.jl")
+include("dynamics/run_speedy.jl")
 
-# if !FLAG_EXTENDED_TESTS && !FLAG_ENZYME_TESTS
-#     @info "Running general test suite"
+# VERTICAL LEVELS
+include("dynamics/vertical_coordinates.jl")
+include("dynamics/geopotential.jl")
 
-    # GENERAL
-    include("spectral_grid.jl")
-    include("parameters.jl")
+# PARAMETERIZATIONS
+include("parameterizations/variables.jl")
+include("parameterizations/custom_parameterization.jl")
+include("parameterizations/zenith.jl")
+include("parameterizations/land_sea_mask.jl")
+include("parameterizations/ocean_sea_ice.jl")
+include("parameterizations/large_scale_condensation.jl")
+include("parameterizations/convection.jl")
+include("parameterizations/albedo.jl")
+include("parameterizations/land.jl")
+include("parameterizations/longwave_radiation.jl")
+include("parameterizations/shortwave_radiation.jl")
+include("parameterizations/surface_fluxes.jl")
+include("parameterizations/random_process.jl")
+include("parameterizations/stochastic_physics.jl")
+include("parameterizations/all_parametrizations.jl")
 
-    # GPU/KERNELABSTRACTIONS
-    include("GPU/kernelabstractions.jl")
-
-    # DYNAMICS
-    include("dynamics/horizontal_diffusion.jl")
-    include("dynamics/time_stepping.jl")
-    include("dynamics/vertical_advection.jl")
-    include("dynamics/particles.jl")
-    include("dynamics/particle_advection.jl")
-    include("dynamics/forcing_drag.jl")
-    include("dynamics/dates.jl")
-    include("dynamics/set.jl")
-    include("dynamics/copy_variables.jl")
-    include("dynamics/orography.jl")
-    include("dynamics/initial_conditions.jl")
-    include("dynamics/run_speedy.jl")
-
-    # VERTICAL LEVELS
-    include("dynamics/vertical_coordinates.jl")
-    include("dynamics/geopotential.jl")
-
-    # PARAMETERIZATIONS
-    include("parameterizations/variables.jl")
-    include("parameterizations/custom_parameterization.jl")
-    include("parameterizations/zenith.jl")
-    include("parameterizations/land_sea_mask.jl")
-    include("parameterizations/ocean_sea_ice.jl")
-    include("parameterizations/large_scale_condensation.jl")
-    include("parameterizations/convection.jl")
-    include("parameterizations/albedo.jl")
-    include("parameterizations/land.jl")
-    include("parameterizations/longwave_radiation.jl")
-    include("parameterizations/shortwave_radiation.jl")
-    include("parameterizations/surface_fluxes.jl")
-    include("parameterizations/random_process.jl")
-    include("parameterizations/stochastic_physics.jl")
-    include("parameterizations/all_parametrizations.jl")
-
-    # OUTPUT/EXTENSION
-    include("output/callbacks.jl")
-    include("output/schedule.jl")
-    include("output/netcdf_output.jl")
-    include("output/jld2_output.jl")
-    include("output/feedback.jl")
-# end
+# OUTPUT/EXTENSION
+include("output/callbacks.jl")
+include("output/schedule.jl")
+include("output/netcdf_output.jl")
+include("output/jld2_output.jl")
+include("output/feedback.jl")
