e70dc8d097e5172ad8caec767c873e23bb495c20
Max <maximilian.gelbrecht@posteo.de>
2026-03-30 07:44:18 +0200

    adjust reactant tests

--- SpeedyWeather/test/reactant/setup.jl
+++ SpeedyWeather/test/reactant/setup.jl
@@ -6,29 +6,12 @@ nlayers_for_model(::Type{ShallowWaterModel}) = 1
 nlayers_for_model(::Type{PrimitiveDryModel}) = 8
 nlayers_for_model(::Type{PrimitiveWetModel}) = 8
 
-"""Get default initial conditions for a given model type."""
-function default_initial_conditions(::Type{BarotropicModel})
-    return InitialConditions(; vordiv = ZeroInitially())
-end
-
-function default_initial_conditions(::Type{ShallowWaterModel})
-    return InitialConditions(; vordiv = ZonalJet())
-end
-
-function default_initial_conditions(::Type{PrimitiveDryModel})
-    return InitialConditions(; vordiv = ZonalWind())
-end
-
-function default_initial_conditions(::Type{PrimitiveWetModel})
-    return InitialConditions(; vordiv = ZonalWind())
-end
-
 """Create a CPU model of the given type."""
 function create_cpu_model(ModelType::Type; trunc = TRUNC, kwargs...)
     nlayers = nlayers_for_model(ModelType)
     spectral_grid = SpectralGrid(; nlayers, trunc)
     M = MatrixSpectralTransform(spectral_grid)
-    initial_conditions = default_initial_conditions(ModelType)
+    initial_conditions = InitialConditions(spectral_grid, ModelType)
     return ModelType(spectral_grid; spectral_transform = M, feedback = nothing, initial_conditions, kwargs...)
 end
 
@@ -38,6 +21,6 @@ function create_reactant_model(ModelType::Type; trunc = TRUNC, kwargs...)
     arch = SpeedyWeather.ReactantDevice()
     spectral_grid = SpectralGrid(; architecture = arch, nlayers, trunc)
     M = MatrixSpectralTransform(spectral_grid)
-    initial_conditions = default_initial_conditions(ModelType)
+    initial_conditions = InitialConditions(spectral_grid, ModelType)
     return ModelType(spectral_grid; spectral_transform = M, feedback = nothing, initial_conditions, kwargs...)
 end
