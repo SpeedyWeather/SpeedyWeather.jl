38ff52d88e7d61ec0ca95b92603cf3e2df77a9fc
Max <maximilian.gelbrecht@posteo.de>
2026-04-21 11:45:47 +0200

    include gpu setup

--- SpeedyWeather/test/reactant/setup.jl
+++ SpeedyWeather/test/reactant/setup.jl
@@ -12,7 +12,7 @@ function create_cpu_model(ModelType::Type; trunc = TRUNC, kwargs...)
     spectral_grid = SpectralGrid(; nlayers, trunc)
     M = MatrixSpectralTransform(spectral_grid)
     initial_conditions = InitialConditions(spectral_grid, ModelType)
-    return ModelType(spectral_grid; spectral_transform = M, feedback = nothing, dynamics=true, initial_conditions, convection=nothing, kwargs...)
+    return ModelType(spectral_grid; spectral_transform = M, feedback = nothing, dynamics=false, dynamics_only=false, initial_conditions, convection=nothing, kwargs...)
 end
 
 """Create a Reactant model of the given type."""
@@ -22,5 +22,15 @@ function create_reactant_model(ModelType::Type; trunc = TRUNC, kwargs...)
     spectral_grid = SpectralGrid(; architecture = arch, nlayers, trunc)
     M = MatrixSpectralTransform(spectral_grid)
     initial_conditions = InitialConditions(spectral_grid, ModelType)
-    return ModelType(spectral_grid; spectral_transform = M, feedback = nothing, dynamics=true, initial_conditions, convection=nothing, kwargs...)
+    return ModelType(spectral_grid; spectral_transform = M, feedback = nothing, dynamics=false, dynamics_only=false, initial_conditions, convection=nothing, kwargs...)
+end
+
+"""Creat a (regular) GPU model of the given type."""
+function create_gpu_model(ModelType::Type; trunc = TRUNC, kwargs...)
+     nlayers = nlayers_for_model(ModelType)
+    arch = SpeedyWeather.GPU()
+    spectral_grid = SpectralGrid(; architecture = arch, nlayers, trunc)
+    M = MatrixSpectralTransform(spectral_grid)
+    initial_conditions = InitialConditions(spectral_grid, ModelType)
+    return ModelType(spectral_grid; spectral_transform = M, feedback = nothing, dynamics=false, dynamics_only=false, initial_conditions, convection=nothing, kwargs...)
 end
