b1aed0e32b489462ac34188a93ecaa75a7e16c7b
Milan Kloewer <milankloewer@gmx.de>
2026-05-14 15:53:29 +0100

    use get_output_path

--- SpeedyWeather/ext/SpeedyWeatherBrowzarrExt.jl
+++ SpeedyWeather/ext/SpeedyWeatherBrowzarrExt.jl
@@ -4,11 +4,8 @@ using SpeedyWeather
 using Browzarr
 
 function Browzarr.browzarr(simulation::SpeedyWeather.AbstractSimulation; kwargs...)
-    (; output) = simulation.model
-    @assert output.active "Output has not been stored (yet) for this simulation. `run!(simulation)` with `output=true`."
-    
     # for netCDF it's a file, for zarr it's a directory call both "store"
-    store_path = joinpath(output.run_path, output.filename)
+    store_path = SpeedyWeather.get_output_path(simulation)
     return Browzarr.browzarr(; store = store_path, kwargs...)
 end
 
