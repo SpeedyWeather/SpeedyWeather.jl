03b81db715389715edb1f9dfb81ed163ac7f40fe
Maximilian Gelbrecht <maxgelbr@pik-potsdam.de>
2026-05-21 14:33:59 +0200

    update reactant gpu ci test

--- SpeedyWeather/test/GPU/reactant.jl
+++ SpeedyWeather/test/GPU/reactant.jl
@@ -7,19 +7,31 @@ Reactant.set_default_backend("gpu")
 arch = SpeedyWeather.ReactantDevice()
 spectral_grid = SpectralGrid(architecture = arch, nlayers = 1)
 spectral_transform = MatrixSpectralTransform(spectral_grid)
-model = BarotropicModel(spectral_grid = spectral_grid, spectral_transform = spectral_transform)
+model = BarotropicModel(
+    spectral_grid = spectral_grid,
+    spectral_transform = spectral_transform,
+    output = nothing,
+    feedback = nothing,
+)
 simulation = initialize!(model)
 run!(simulation, steps = 10)
 
-@test !any(isnan.(simulation.prognostic_variables.vorticity))
+@test !any(isnan.(simulation.variables.prognostic.vorticity))
 
 # also test the PrimitiveWetModel (currentlty convection isn't adjusted yet)
 spectral_grid = SpectralGrid(architecture = arch)
 spectral_transform = MatrixSpectralTransform(spectral_grid)
 longwave_radiation = OneBandLongwave(spectral_grid, transmissivity = ConstantLongwaveTransmissivity(spectral_grid))
 
-model = PrimitiveWetModel(spectral_grid = spectral_grid, spectral_transform = spectral_transform, convection=nothing, feedback = nothing)
+model = PrimitiveWetModel(
+    spectral_grid = spectral_grid,
+    spectral_transform = spectral_transform,
+    convection = nothing,
+    feedback = nothing,
+    output = nothing,
+    longwave_radiation = longwave_radiation,
+)
 simulation = initialize!(model)
 run!(simulation, steps = 10)
 
-@test !any(isnan.(simulation.prognostic_variables.vorticity))
+@test !any(isnan.(simulation.variables.prognostic.vorticity))
