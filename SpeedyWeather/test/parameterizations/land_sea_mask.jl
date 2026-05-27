a1b746d6e48fad85d624c89ceb6f91cdd05bdbc3
Max <maximilian.gelbrecht@posteo.de>
2026-05-27 10:26:36 +0200

    forgot about land sea mask

--- SpeedyWeather/test/parameterizations/land_sea_mask.jl
+++ SpeedyWeather/test/parameterizations/land_sea_mask.jl
@@ -3,9 +3,8 @@
         spectral_grid = SpectralGrid(trunc = 31, nlayers = 8)
         mask = Mask(spectral_grid)
         model = PrimitiveWetModel(spectral_grid; land_sea_mask = mask)
-        simulation = initialize!(model)
-        model.feedback.verbose = false
+        simulation = initialize!(model; feedback = Feedback(verbose = false))
         run!(simulation, period = Day(5))
-        @test simulation.model.feedback.nans_detected == false
+        @test simulation.feedback.nans_detected == false
     end
 end
