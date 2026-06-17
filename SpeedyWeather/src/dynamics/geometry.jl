38842df7ae9c35a24c5b987cda650c97e1309803
Milan Kloewer <milankloewer@gmx.de>
2026-06-17 15:14:02 +0100

    First ideas for SigmaPressureCoordinates

--- SpeedyWeather/src/dynamics/geometry.jl
+++ SpeedyWeather/src/dynamics/geometry.jl
@@ -1,4 +1,4 @@
-abstract type AbstractGeometry end
+abstract type AbstractGeometry <: AbstractModelComponent end
 export Geometry
 
 """
@@ -89,12 +89,6 @@ $(TYPEDFIELDS)
 
     "σ level thicknesses, σₖ₊₁ - σₖ"
     σ_levels_thick::VectorType = σ_levels_half[2:end] - σ_levels_half[1:(end - 1)]
-
-    "log of σ at full levels, include surface (σ=1) as last element"
-    ln_σ_levels_full::VectorType = log.(vcat(σ_levels_full, 1))
-
-    "Full to half levels interpolation"
-    full_to_half_interpolation::VectorType = σ_interpolation_weights(σ_levels_full, σ_levels_half)
 end
 
 Adapt.@adapt_structure Geometry
@@ -122,60 +116,4 @@ end
 function initialize!(geometry::Geometry, model::AbstractModel)
     geometry.radius[] = model.planet.radius
     return geometry
-end
-
-"""
-$(TYPEDSIGNATURES)
-Interpolation weights for full to half level interpolation
-on sigma coordinates. Following Fortran SPEEDY documentation eq. (1)."""
-function σ_interpolation_weights(
-        σ_levels_full::AbstractVector,
-        σ_levels_half::AbstractVector
-    )
-
-    weights = zero(σ_levels_full)
-    nlayers = length(weights)
-    nlayers == 1 && return weights     # escape early for 1 layer to avoid out-of-bounds access
-
-    for k in 1:(nlayers - 1)
-        weights[k] = (log(σ_levels_half[k + 1]) - log(σ_levels_full[k])) /
-            (log(σ_levels_full[k + 1]) - log(σ_levels_full[k]))
-    end
-    # was log(0.99) in Fortran SPEEDY code but doesn't make sense to me
-    weights[end] = (log(σ_levels_half[nlayers + 1]) - log(σ_levels_full[nlayers])) /
-        (log(σ_levels_full[nlayers]) - log(σ_levels_full[nlayers - 1]))
-
-    return weights
-end
-
-
-"""
-$(TYPEDSIGNATURES)
-Given a vector in column defined at full levels, do a linear interpolation in
-log(σ) to calculate its values at half-levels, skipping top (k=1/2), extrapolating to bottom (k=nlayers+1/2).
-"""
-function vertical_interpolate!(
-        A_half::Vector,             # quantity A on half levels (excl top)
-        A_full::Vector,             # quantity A on full levels
-        G::Geometry,
-    )
-    nlayers = length(A_half)
-    weights = G.full_to_half_interpolation
-
-    # full levels contain one more for surface
-    # TODO this is currently confusing because the surface fluxes use full[end]
-    # as surface value which is technically on half levels though!
-    @boundscheck nlayers <= length(A_full) || throw(BoundsError)
-    @boundscheck nlayers <= length(weights) || throw(BoundsError)
-
-    # For A at each full level k, compute A at the half-level below, i.e. at the boundary
-    # between the full levels k and k+1. Fortran SPEEDY documentation eq. (1)
-    for k in 1:(nlayers - 1)
-        A_half[k] = A_full[k] + weights[k] * (A_full[k + 1] - A_full[k])
-    end
-
-    # Compute the values at the surface separately
-    A_half[nlayers] = A_full[nlayers] + weights[nlayers] * (A_full[nlayers] - A_full[nlayers - 1])
-
-    return nothing
 end
--- SpeedyWeather/src/dynamics/vertical_coordinates.jl
+++ SpeedyWeather/src/dynamics/vertical_coordinates.jl
@@ -1,4 +1,4 @@
-abstract type AbstractVerticalCoordinate end
+abstract type AbstractVerticalCoordinate <: AbstractModelComponent end
 
 export SigmaCoordinates
 
@@ -23,25 +23,24 @@ SigmaCoordinates(σ_half::AbstractVector) = SigmaCoordinates(length(σ_half) - 1
 # constructor using default nlayers if nothing provided
 SigmaCoordinates() = SigmaCoordinates(DEFAULT_NLAYERS)
 
-
 function Base.show(io::IO, σ::SigmaCoordinates)
     println(io, "$(σ.nlayers)-layer $(typeof(σ))")
     nchars = length(string(σ.nlayers))
+    σ_half = on_architecture(CPU(), σ.σ_half)
     format = Printf.Format("%$(nchars)d")
     for k in 1:σ.nlayers
-        println(io, "├─ ", @sprintf("%1.4f", σ.σ_half[k]), "  k = ", Printf.format(format, k - 1), ".5")
-        σk = (σ.σ_half[k] + σ.σ_half[k + 1]) / 2
+        println(io, "├─ ", @sprintf("%1.4f", σ_half[k]), "  k = ", Printf.format(format, k - 1), ".5")
+        σk = (σ_half[k] + σ_half[k + 1]) / 2
         println(io, "│× ", @sprintf("%1.4f", σk), "  k = ", Printf.format(format, k))
     end
-    return print(io, "└─ ", @sprintf("%1.4f", σ.σ_half[end]), "  k = ", Printf.format(format, σ.nlayers), ".5")
+    return print(io, "└─ ", @sprintf("%1.4f", σ_half[end]), "  k = ", Printf.format(format, σ.nlayers), ".5")
 end
 
-"""
-$(TYPEDSIGNATURES)
+"""$(TYPEDSIGNATURES)
 Vertical sigma coordinates defined by their nlayers+1 half levels `σ_levels_half`. Sigma coordinates are
 fraction of surface pressure (p/p0) and are sorted from top (stratosphere) to bottom (surface).
 The first half level is at 0 the last at 1. Default levels are equally spaced from 0 to 1 (including)."""
-function default_sigma_coordinates(nlayers::Integer)
+function default_sigma_coordinates(nlayers::Integer, profile = z -> z)
 
     # equi-distant in σ
     σ_half = collect(range(0, 1, nlayers + 1))
@@ -53,8 +52,7 @@ function default_sigma_coordinates(nlayers::Integer)
     return σ_half
 end
 
-"""
-$(TYPEDSIGNATURES)
+"""$(TYPEDSIGNATURES)
 Check that nlayers and σ_half match."""
 function sigma_okay(nlayers::Integer, σ_half::AbstractVector)
     @assert σ_half[1] >= 0 "First manually specified σ_half has to be >0"
@@ -62,4 +60,51 @@ function sigma_okay(nlayers::Integer, σ_half::AbstractVector)
     @assert nlayers == (length(σ_half) - 1) "nlayers has to be length of σ_half - 1"
     @assert Utils.isincreasing(σ_half) "Vertical sigma coordinates are not increasing."
     return true
+end
+
+export SigmaPressureCoordinates
+struct SigmaPressureCoordinates{NF, IntType, VectorType, F} <: AbstractVerticalCoordinate
+    reference_pressure::NF
+    A_half::VectorType
+    B_half::VectorType
+    A_full::VectorType
+    B_full::VectorType
+end
+
+function SigmaPressureCoordinates(
+    spectral_grid::SpectralGrid;
+    reference_pressure = 1e5,
+    σ_half = range(0, 1, length=spectral_grid.nlayers+1),
+    transition = σ -> σ
+)
+    # sigma coordinates
+    σ_half = on_architecture(spectral_grid.architecture, σ_half)
+    σ_full = 0.5 * (σ_half[2:end] + σ_half[1:(end - 1)])
+
+    # hybrid coordinates
+    A_half = @. σ_half * (1 - transition(σ_half))
+    B_half = @. σ_half * transition(σ_half)
+    A_full = @. σ_full * (1 - transition(σ_full))
+    B_full = @. σ_full * transition(σ_full)
+
+    p_ref = convert(spectral_grid.NF, reference_pressure)
+    return SigmaPressureCoordinates(p_ref, A_half, B_half, A_full, B_full)
+end
+
+# function SigmaPressureCoordinateA(σ, SP::SigmaPressureCoordinates)
+#     γ = SP.transition
+#     return σ * (1 - γ(σ)) * SP.reference_pressure + σ
+
+function pressure(surface_pressure::Number, k::Integer, coordinate::SigmaPressureCoordinates)
+    A = coordinate.A_full
+    B = coordinate.B_full
+    p_ref = coordinate.reference_pressure
+    return A[k]*p_ref + B[k]*surface_pressure
+end
+
+function pressure_thickness(surface_pressure::Number, k::Integer, coordinate::SigmaPressureCoordinates)
+    A = coordinate.A_half
+    B = coordinate.B_half
+    p_ref = coordinate.reference_pressure
+    return (A[k+1] - A[k])*p_ref + (B[k+1] - B[k])*surface_pressure
 end
