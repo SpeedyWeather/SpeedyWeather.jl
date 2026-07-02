b3fff140a84ebf1bbc44074bc6737ad8eaf66e9a
Maximilian Gelbrecht <maxgelbr@pik-potsdam.de>
2026-07-02 17:41:17 +0200

    broadcast style fix

--- RingGrids/ext/RingGridsReactantExt.jl
+++ RingGrids/ext/RingGridsReactantExt.jl
@@ -5,6 +5,6 @@ using Reactant
 
 const AnyReactantArray = Union{Reactant.AnyConcretePJRTArray, Reactant.AnyConcreteIFRTArray, Reactant.AnyTracedRArray}
 
-Base.Broadcast.BroadcastStyle(::Type{AbstractField{T, N, ArrayType, Grid}}) where {T, N, ArrayType <: AnyReactantArray, Grid} = Base.Broadcast.BroadcastStyle(ArrayType)
+Base.Broadcast.BroadcastStyle(::Type{F}) where {F <: AbstractField{T, N, ArrayType, Grid}} where {T, N, ArrayType <: AnyReactantArray, Grid} = Base.Broadcast.BroadcastStyle(ArrayType)
 
 end
