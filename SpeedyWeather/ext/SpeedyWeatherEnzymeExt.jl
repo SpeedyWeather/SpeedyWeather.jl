20a82cac889627b0f725d7822a3003379bad7673
Max <maximilian.gelbrecht@posteo.de>
2026-07-03 22:04:57 +0200

    maxtypeoffset!

--- SpeedyWeather/ext/SpeedyWeatherEnzymeExt.jl
+++ SpeedyWeather/ext/SpeedyWeatherEnzymeExt.jl
@@ -4,6 +4,17 @@ using SpeedyWeather
 using Enzyme
 using SpeedyWeather.ProgressMeter
 
+function __init__()
+    # On Julia > 1.10, Enzyme's type analysis fails on large aggregates 
+    # differentiated kernels (EnzymeNoTypeError, see https://github.com/EnzymeAD/Enzyme.jl/issues/3275).
+    # We can fix this by setting a high maxtypeoffset (4096), but only on Julia 1.11+
+    if VERSION >= v"1.11"
+        Enzyme.API.maxtypeoffset!(4096)
+    end
+    return nothing
+end
+
+###
 # implement make_zero where the default one fails
 
 # this lock is part of the ProgressMeter that's part of the Feedback of all models
