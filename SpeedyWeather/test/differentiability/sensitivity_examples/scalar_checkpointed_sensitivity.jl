f09410a197e697beaae6763315ddf21805046663
Max <maximilian.gelbrecht@posteo.de>
2026-08-13 10:18:47 +0200

    update scalar one

--- SpeedyWeather/test/differentiability/sensitivity_examples/scalar_checkpointed_sensitivity.jl
+++ SpeedyWeather/test/differentiability/sensitivity_examples/scalar_checkpointed_sensitivity.jl
@@ -28,7 +28,7 @@ function checkpointed_timesteps!(vars::Variables, model, N_steps, checkpoint_sch
         SpeedyWeather.time_step!(vars.prognostic.clock, model.time_stepping)                # then step the clock forward
     end
 
-    return diagn.grid.temp_grid[i_point, 8]
+    return vars.grid.temperature[i_point, 8]
 end
 
 checkpoint_scheme = Revolve(N)
