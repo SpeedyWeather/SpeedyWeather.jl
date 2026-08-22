45919def277cd75df38713fbc2a3c643e88f01ed
Gregor Decristoforo <gregor.decristoforo@gmail.com>
2026-08-22 15:47:37 +0200

    Avoid a keyword-argument dispatch ( dead branch) in the particle-initialization kernel

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## Unreleased
 
+- Avoid a keyword-argument dispatch (`Base.kwerr` dead branch) in the particle-initialization kernel that triggered AMDGPU hostcalls [#TODO](https://github.com/SpeedyWeather/SpeedyWeather.jl/pull/TODO)
 - Enable HIP-graphs acceleration of the batched Fourier transform for AMDGPU, mirroring the existing CUDA-graphs implementation [#1218](https://github.com/SpeedyWeather/SpeedyWeather.jl/pull/1218)
 - Reformulate trigonometric functions to avoid hostcalls on AMDGPU - [#1210](https://github.com/SpeedyWeather/SpeedyWeather.jl/pull/1210)
 
--- SpeedyWeather/src/dynamics/particle_advection.jl
+++ SpeedyWeather/src/dynamics/particle_advection.jl
@@ -147,7 +147,11 @@ end
 
     # modulo all particles here
     # i.e. one can start with a particle at -120˚E which moduloed to 240˚E here
-    particles[i] = mod(set(particles[i]; σ = σ))
+    # NB: avoid `set(particle; σ = σ)` here (keyword-argument dispatch triggers
+    # AMDGPU hostcalls, see `sind`/`cosd` note in `advect_2D` above); construct
+    # the updated particle positionally instead
+    particle = particles[i]
+    particles[i] = mod(typeof(particle)(particle.active, particle.lon, particle.lat, σ))
     lons[i] = particles[i].lon
     lats[i] = particles[i].lat
 end
